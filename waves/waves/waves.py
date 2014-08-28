2c6b1ed15dfdd10376218542d3ce6b152f2c069d
Michael Zingale <michael.zingale@stonybrook.edu>
2014-08-27 21:04:21 -0400

    lots of clean-ups -- use arrow and annotate now

--- waves/waves/waves.py
+++ waves/waves/waves.py
@@ -1,10 +1,12 @@
+#!/bin/env python
+
 import math
 import numpy
 import pylab
 
 # illustrate the difference between wavelength and frequency
 
-# M. Zingale (2009-01-28)
+# M. Zingale 
 
 
 def waves():
@@ -27,9 +29,7 @@ def waves():
     iframe = 0
 
     while (t < tmax):
- 
-
-        
+         
         pylab.clf()
 
         pylab.subplot(211)
@@ -39,21 +39,21 @@ def waves():
         xpt = 2.0
         ypt = math.sin(2*math.pi*(xpt - v*t)/lambda_1)
         
-
         pylab.plot(x,y,color="b")
         pylab.scatter([xpt],[ypt],color="b")
 
         # draw and annotate a dimension line
-        pylab.plot([xmin,xmin],[1.1,1.3],color='k')
-        pylab.plot([xmin+lambda_1,xmin+lambda_1],[1.1,1.3],color='k')
-        pylab.plot([xmin,xmin+lambda_1],[1.2,1.2],color='k')
-        pylab.text(0.5*(xmin + xmin + lambda_1), 1.4, r"$\lambda = %3.2f\, \rm{cm}$" % (lambda_1), horizontalalignment='center')
+        pylab.annotate("", (xmin, 1.2), (xmin+lambda_1, 1.2),
+                       arrowprops=dict(arrowstyle="|-|",mutation_scale=5.0))
 
-        pylab.plot([2.0,2.0],[-1.5,1.5],"k--")
+        pylab.text(0.5*(xmin + xmin + lambda_1), 1.4, 
+                   r"$\lambda = %3.2f\, \rm{cm}$" % (lambda_1), 
+                   horizontalalignment='center')
+
+        pylab.plot([2.0,2.0], [-1.5,1.5], "k--")
 
         pylab.axis([xmin,xmax,-1.6,1.6])
         pylab.axis("off")
-
 
         pylab.subplot(212)
 
@@ -66,19 +66,23 @@ def waves():
         pylab.scatter([xpt],[ypt],color="r")
 
         # draw and annotate a dimension line
-        pylab.plot([xmin,xmin],[1.1,1.3],color='k')
-        pylab.plot([xmin+lambda_2,xmin+lambda_2],[1.1,1.3],color='k')
-        pylab.plot([xmin,xmin+lambda_2],[1.2,1.2],color='k')
-        pylab.text(0.5*(xmin + xmin + lambda_2), 1.4, r"$\lambda = %3.2f\, \rm{cm}$" % (lambda_2), horizontalalignment='center')
+        pylab.annotate("", (xmin, 1.2), (xmin+lambda_2, 1.2),
+                       arrowprops=dict(arrowstyle="|-|",mutation_scale=5.0))
 
-        pylab.plot([2.0,2.0],[-1.5,1.5],"k--")
+        pylab.text(0.5*(xmin + xmin + lambda_2), 1.4, 
+                   r"$\lambda = %3.2f\, \rm{cm}$" % (lambda_2), 
+                   horizontalalignment='center')
 
-        # draw the velocity vector -- I should be able to do this with 
-        # the pylab annotate function, but I couldn't get it to work.
-        pylab.plot( [0.75*xmax,0.88*xmax], [-1.5,-1.5], color='k')
-        pylab.plot( [0.845*xmax,0.88*xmax], [-1.6,-1.5], color='k')
-        pylab.plot( [0.845*xmax,0.88*xmax], [-1.4,-1.5], color='k')
-        pylab.text( 0.90*xmax, -1.5, r"$v = %3.2f\, \rm{cm/s}$" % (v), verticalalignment='center')
+        pylab.plot([2.0,2.0], [-1.5,1.5], "k--")
+
+        # draw the velocity vector 
+        pylab.arrow(0.75*xmax, -1.5, 0.13*xmax, 0.0, color="k",
+                    length_includes_head=True,
+                    head_width = 0.15, head_length = 0.15, width=0.05)
+
+        pylab.text( 0.90*xmax, -1.5, 
+                    r"$v = %3.2f\, \rm{cm/s}$" % (v), 
+                    verticalalignment='center')
 
         pylab.axis([xmin,xmax,-1.6,1.6])
         pylab.axis("off")
@@ -86,7 +90,7 @@ def waves():
         pylab.text(0.01,-1.6, r"$t = %5.3f\, \rm{s}$" % (t))
 
         f = pylab.gcf()
-        f.set_size_inches(6.0,6.0)
+        f.set_size_inches(7.2,7.2)
 
         outfile = "wave_%04d.png" % iframe
         pylab.savefig(outfile)
