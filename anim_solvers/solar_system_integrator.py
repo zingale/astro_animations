cef718f3244ac276a1ce48e41669452842ce0f02
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-29 13:50:16 -0500

    add constants

--- anim_solvers/solar_system_integrator.py
+++ anim_solvers/solar_system_integrator.py
@@ -21,6 +21,13 @@ class _PlanetHistory:
         self.vy = np.zeros(num_steps, np.float64)
 
 
+# CGS units
+G = 6.67428e-8        # cm^3 g^{-1} s^{-2}
+M_sun = 1.98892e33    # g
+AU = 1.49598e13       # cm
+year = 3.1556926e7    # s
+
+
 class SolarSystem:
     """
     Integrate a solar system with the Sun at the center and an
