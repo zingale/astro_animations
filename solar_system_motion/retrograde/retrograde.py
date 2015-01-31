07fad25322949d698b6dbfa29a7ecd3b9e27cec7
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-31 14:41:43 -0500

    remove an extraneous n += 1, some import modernization

--- solar_system_motion/retrograde/retrograde.py
+++ solar_system_motion/retrograde/retrograde.py
@@ -1,8 +1,7 @@
 #!/bin/env python
 
 import math
-import numpy
-import pylab
+import matplotlib.pyplot as plt
 
 import anim_solvers.solar_system_integrator as solar_system_integrator
 
@@ -31,7 +30,7 @@ def doit():
     # Earth initialization
 
     # set the initial conditions.  The initial position is perihelion
-    #y[0] = a_E*(1.0 - ecc_E)   # x 
+    #y[0] = a_E*(1.0 - ecc_E)   # x
     #y[1] = 0.0                 # y
 
     # at perihelion, all the veloicity is in the y-direction.
@@ -45,16 +44,16 @@ def doit():
     # Mars initialization
 
     # set the initial conditions.  The initial position is perihelion
-    #y[4] = a_M*(1.0 - ecc_M)  # x 
+    #y[4] = a_M*(1.0 - ecc_M)  # x
     #y[5] = 0.0                 # y
-    
+
     # at perihelion, all the veloicity is in the y-direction.
     #y[6] = 0.0      # v_x
-    
+
     # v_y^2 = (GM/a) (1+e)/(1-e)  (see C&O Eq. 2.33 for example)
     # This is the perihelion velocity.  For a = 1, e = 0, v = 2 pi
     #y[7] = -math.sqrt( (G*M_sun/a_M) * (1.0 + ecc_M) / (1.0 - ecc_M))
-    
+
 
     # These initial conditions were found by putting Mars and Earth
     # both at their perihelion, but with their velocities in the
@@ -64,7 +63,7 @@ def doit():
     # year (91 steps) to get these starting coordinates (note, we reverse
     # the direction of the velocity to get it orbitting in the forward
     # direction.)
-    
+
 
     # Earth
     x  = -0.04631900088483218
@@ -84,8 +83,8 @@ def doit():
 
     vp0 = solar_system_integrator.PlanetPosVel(x, y, vx, vy)
     s.add_planet(a_M, ecc_M, loc="specify", pos_vel=vp0)
-    
-    
+
+
     # integrate
     sol = s.integrate(nsteps_year, nyears)
 
@@ -94,20 +93,20 @@ def doit():
     # plotting
     # ================================================================
     for n in range(len(sol[0].x)):
-        
-        f = pylab.figure()
+
+        f = plt.figure()
 
         # plot the foci
-        pylab.scatter([0], [0], s=250,marker=(5,1), color="k")
-        pylab.scatter([0], [0], s=200,marker=(5,1), color="y")
+        plt.scatter([0], [0], s=250,marker=(5,1), color="k")
+        plt.scatter([0], [0], s=200,marker=(5,1), color="y")
 
         # plot Earth
-        pylab.plot(sol[0].x, sol[0].y, color="b")
-        pylab.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="b")
+        plt.plot(sol[0].x, sol[0].y, color="b")
+        plt.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="b")
 
         # plot Mars
-        pylab.plot(sol[1].x, sol[1].y, color="r")
-        pylab.scatter([sol[1].x[n]],[sol[1].y[n]], s=100, color="r")
+        plt.plot(sol[1].x, sol[1].y, color="r")
+        plt.scatter([sol[1].x[n]],[sol[1].y[n]], s=100, color="r")
 
         # draw a line connecting Earth and Mars and extending a bit
         # further out
@@ -115,38 +114,32 @@ def doit():
         xpt = 3.5
         ypt = sol[0].y[n] + slope*(xpt - sol[0].x[n])
 
-        pylab.plot([sol[0].x[n], xpt], [sol[0].y[n], ypt], "b--")
+        plt.plot([sol[0].x[n], xpt], [sol[0].y[n], ypt], "b--")
 
         # draw some random background stars
-        pylab.scatter([3.2],[ 1.9],s=200,marker=(5,1),color="c")        
-        pylab.scatter([3.6],[ 1.0],s=200,marker=(5,1),color="c")        
-        pylab.scatter([3.4],[-0.4],s=200,marker=(5,1),color="c")        
-        pylab.scatter([3.8],[-0.9],s=200,marker=(5,1),color="c")        
-        pylab.scatter([3.1],[-1.3],s=200,marker=(5,1),color="c")        
+        plt.scatter([3.2],[ 1.9],s=200,marker=(5,1),color="c")
+        plt.scatter([3.6],[ 1.0],s=200,marker=(5,1),color="c")
+        plt.scatter([3.4],[-0.4],s=200,marker=(5,1),color="c")
+        plt.scatter([3.8],[-0.9],s=200,marker=(5,1),color="c")
+        plt.scatter([3.1],[-1.3],s=200,marker=(5,1),color="c")
 
-        pylab.axis([-2.,4.,-1.5,2.0])
-        pylab.axis("off")
+        plt.axis([-2.,4.,-1.5,2.0])
+        plt.axis("off")
 
-        ax = pylab.gca()
+        ax = plt.gca()
         ax.set_aspect("equal", "datalim")
 
         f.set_size_inches(12.8,7.2)
 
-        pylab.xlabel("AU")
-        pylab.ylabel("AU")
-        pylab.text(-1.5,-1.5, "time = %6.3f yr" % sol[0].t[n])
-        pylab.title("Retrograde Mars")
+        plt.xlabel("AU")
+        plt.ylabel("AU")
+        plt.text(-1.5,-1.5, "time = %6.3f yr" % sol[0].t[n])
+        plt.title("Retrograde Mars")
 
-        pylab.savefig("retrograde_%04d.png" % n)
+        plt.savefig("retrograde_%04d.png" % n)
 
-        pylab.close()
+        plt.close()
 
-        n += 1
 
-    
 if __name__== "__main__":
     doit()
-
-
-    
-        
