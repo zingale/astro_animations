a9c1a57d36734ccdff8f915f196ad0f3eeb81dc4
Michael Zingale <michael.zingale@stonybrook.edu>
2015-02-25 20:38:38 -0500

    more work on the tidal exampe

--- solar_system_motion/tidal_locking/tidal_locking.py
+++ solar_system_motion/tidal_locking/tidal_locking.py
@@ -12,14 +12,30 @@ class Circle:
         self.y = R*np.sin(theta)
         
 
+class Ellipse:
+    def __init__(self, a, e, rot):
+        theta = np.radians(np.arange(361))
+        # polar relative to a foci
+        r = a*(1.0-e**2)/(1.0+e*np.cos(theta))
 
+        self.x = r*np.cos(theta)
+        self.y = r*np.sin(theta)
+
+        # shift to be with respect to center
+        self.x += a*e
+
+        # rotate it
+        for n in range(len(self.x)):
+            self.x[n], self.y[n] = sf._rotate([self.x[n], self.y[n]], (0.,0.), rot)
+
+            
 def doit():
 
 
-    N = 360
+    N = 720
 
     P_orbit = 1.0
-    P_rotate = 0.1
+    P_rotate = 0.2
 
     R_orbit = 25.0
 
@@ -32,11 +48,13 @@ def doit():
     R_moon = 2.5
     R_planet = 10.0
 
-    L = 1.5   # height of person
+    L = 2.0   # height of person
     
     moon = Circle(R_moon)
+
     planet = Circle(R_planet)    
 
+        
     dt = P_orbit/float(N)
     
     for n in range(N):
@@ -49,7 +67,7 @@ def doit():
         plt.clf()
 
         # draw the big planet
-        plt.fill(planet.x, planet.y, color="b")
+        plt.fill(planet.x, planet.y, color="#e6e6b8")
 
         # draw the orbit
         plt.plot(orbit.x, orbit.y, color="0.5", ls=":")
@@ -57,6 +75,12 @@ def doit():
         # draw the moon
         plt.fill(x_m + moon.x, y_m + moon.y, color="0.5")
 
+        # draw the tides
+        tides = Ellipse(R_moon*1.4, 0.6, omega_orbit*t)
+
+        plt.fill(x_m + tides.x, y_m + tides.y, color="c", zorder=-100, alpha=0.5)
+
+        
         # draw the person
         center = ( x_m + (R_moon + 0.5*L)*np.cos(omega_rotate*t),
                    y_m + (R_moon + 0.5*L)*np.sin(omega_rotate*t) )
