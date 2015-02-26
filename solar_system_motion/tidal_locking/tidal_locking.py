2e5ee83cf676b628c053a340c0386a9a09730f5d
Michael Zingale <michael.zingale@stonybrook.edu>
2015-02-25 22:10:08 -0500

    more realistic

--- solar_system_motion/tidal_locking/tidal_locking.py
+++ solar_system_motion/tidal_locking/tidal_locking.py
@@ -28,6 +28,24 @@ class Ellipse:
         for n in range(len(self.x)):
             self.x[n], self.y[n] = sf._rotate([self.x[n], self.y[n]], (0.,0.), rot)
 
+
+def ellipse_point(a, e, theta, rot):
+    """ return the x, y coordinates of a point on a ellipse an angle
+        theta from the x-axis, where theta is measured from the *center*
+        of the ellipse.  Then rotate the point about the center by
+        an angle rot"""
+
+    # semi-minor axis
+    b = a*np.sqrt(1 - e**2)
+
+    # polar form relative to center
+    r = a*b/np.sqrt( (b*np.cos(theta))**2 + (a*np.sin(theta))**2 )
+
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+
+    return sf._rotate([x, y], (0.,0.), rot)
+
             
 def doit():
 
@@ -73,29 +91,54 @@ def doit():
         plt.plot(orbit.x, orbit.y, color="0.5", ls=":")
 
         # draw the moon
-        plt.fill(x_m + moon.x, y_m + moon.y, color="0.5")
+        #plt.fill(x_m + moon.x, y_m + moon.y, color="0.5")
 
         # draw the tides
-        tides = Ellipse(R_moon*1.4, 0.6, omega_orbit*t)
+        a = 1.4*R_moon
+        e = 0.6
+        tides = Ellipse(a, e, omega_orbit*t)
 
-        plt.fill(x_m + tides.x, y_m + tides.y, color="c", zorder=-100, alpha=0.5)
+        plt.fill(x_m + tides.x, y_m + tides.y, color="0.5", zorder=-100, alpha=0.5)
 
         
         # draw the person
-        center = ( x_m + (R_moon + 0.5*L)*np.cos(omega_rotate*t),
-                   y_m + (R_moon + 0.5*L)*np.sin(omega_rotate*t) )
+
+        # find the point on the elliptical moon where the person lies.
+        # The ellipse is already rotated by omega_orbit*t and the
+        # person will be omega_rotate*t wrt the x-axis, so they are
+        # (omega_rotate - omega_orbit)*t from the semi-major axis of
+        # the ellipse.  So we use the polar equation of an ellipse
+        # (wrt center) to find that point and then rotate that point
+        # about the ellipse center to correspond to the current
+        # orientation of the ellipse
+        xp, yp = ellipse_point(a, e, (omega_rotate-omega_orbit)*t, omega_orbit*t)
+
+        # now we extend by the half-height of the person along the
+        # line connecting the person to the center of the ellipse,
+        # since we draw a person by specifying the middle of the
+        # person.
+        xp1 = xp + L/2*np.cos(omega_rotate*t)
+        yp1 = yp + L/2*np.sin(omega_rotate*t)
+        
+        center = (x_m + xp1, y_m + yp1)        
         sf.draw_person(center, L, omega_rotate*t-np.pi/2, color="r")
+
+        # and a line to the person
+        plt.plot([x_m, x_m + xp], [y_m, y_m + yp], "k")
         
         plt.axis("off")
 
         plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
+        max_dist = R_orbit + R_moon + 1.5*L
+        plt.axis([-max_dist, max_dist, -max_dist, max_dist])
+        
         ax = plt.gca()
         ax.set_aspect("equal", "datalim")
         
         f = plt.gcf()
         f.set_size_inches(7.2,7.2)
-                                                         
+    
         plt.savefig("tidal_locking_{:04}.png".format(n))
         
 
