f66ae31de4830cf25ee50ca3f819d1bfb1372047
Michael Zingale <michael.zingale@stonybrook.edu>
2015-02-26 09:32:44 -0500

    annotation

--- solar_system_motion/tidal_locking/tidal_locking.py
+++ solar_system_motion/tidal_locking/tidal_locking.py
@@ -58,7 +58,6 @@ def doit():
     orbit = Circle(R_orbit)
     
     omega_orbit = 2.0*np.pi/P_orbit
-    omega_rotate = 2.0*np.pi/P_rotate
 
     R_moon = 2.5
     R_planet = 10.0
@@ -71,89 +70,114 @@ def doit():
 
         
     dt = P_orbit/float(N)
-    
-    for n in range(N):
 
-        t = n*dt
+    for d, scene in enumerate(["unlocked", "locked"]):
+
+        if scene == "unlocked":
+            omega_rotate = 2.0*np.pi/P_rotate
+        else:
+            omega_rotate = 2.0*np.pi/P_orbit
+            
         
-        x_m = R_orbit*np.cos(omega_orbit*t)
-        y_m = R_orbit*np.sin(omega_orbit*t)
+        for n in range(N):
 
-        plt.clf()
+            t = n*dt
+            
+            x_m = R_orbit*np.cos(omega_orbit*t)
+            y_m = R_orbit*np.sin(omega_orbit*t)
 
-        # draw the big planet
-        plt.fill(planet.x, planet.y, color="#e6e6b8")
+            plt.clf()
 
-        # draw the orbit
-        plt.plot(orbit.x, orbit.y, color="0.5", ls=":")
+            # draw the big planet
+            plt.fill(planet.x, planet.y, color="#e6e6b8")
 
-        # draw the moon
-        #plt.fill(x_m + moon.x, y_m + moon.y, color="0.5")
+            # draw the orbit
+            plt.plot(orbit.x, orbit.y, color="0.5", ls=":")
 
-        # draw the tides
-        a = 1.4*R_moon
-        e = 0.6
+            # draw the moon
+            #plt.fill(x_m + moon.x, y_m + moon.y, color="0.5")
 
-        #tides = Ellipse(a, e, omega_orbit*t)
-        #plt.fill(x_m + tides.x, y_m + tides.y, color="0.5", zorder=-100, alpha=0.5)
+            # draw the tides
+            a = 1.4*R_moon
+            e = 0.6
 
-        # color the quadrants differently to show the rotation more clearly
-        for l, q in enumerate([(0,90), (90,180), (180,270), (270,360)]):
-            theta_range = (q[0] + np.degrees((omega_rotate-omega_orbit)*t),
-                           q[1] + np.degrees((omega_rotate-omega_orbit)*t))
-            tides = Ellipse(a, e, omega_orbit*t, theta_range=theta_range)
-            if l % 2 == 0:
-                c = "m"
-            else:
-                c = "0.5"
+            # color the quadrants differently to show the rotation more clearly
+            for l, q in enumerate([(0,90), (90,180), (180,270), (270,360)]):
+                theta_range = (q[0] + np.degrees((omega_rotate-omega_orbit)*t),
+                               q[1] + np.degrees((omega_rotate-omega_orbit)*t))
+                tides = Ellipse(a, e, omega_orbit*t, theta_range=theta_range)
+                if l % 2 == 0:
+                    c = "m"
+                else:
+                    c = "0.5"
                 
-            plt.fill([x_m] + list(x_m + tides.x) + [x_m],
-                     [y_m] + list(y_m + tides.y) + [y_m],
-                     color=c, zorder=-100, alpha=0.5)
+                plt.fill([x_m] + list(x_m + tides.x) + [x_m],
+                         [y_m] + list(y_m + tides.y) + [y_m],
+                         color=c, zorder=-100, alpha=0.5)
         
         
-        # draw the person
+            # draw the person
 
-        # find the point on the elliptical moon where the person lies.
-        # The ellipse is already rotated by omega_orbit*t and the
-        # person will be omega_rotate*t wrt the x-axis, so they are
-        # (omega_rotate - omega_orbit)*t from the semi-major axis of
-        # the ellipse.  So we use the polar equation of an ellipse
-        # (wrt center) to find that point and then rotate that point
-        # about the ellipse center to correspond to the current
-        # orientation of the ellipse
-        xp, yp = ellipse_point(a, e, (omega_rotate-omega_orbit)*t, omega_orbit*t)
+            # find the point on the elliptical moon where the person
+            # lies.  The ellipse is already rotated by omega_orbit*t
+            # and the person will be omega_rotate*t wrt the x-axis, so
+            # they are (omega_rotate - omega_orbit)*t from the
+            # semi-major axis of the ellipse.  So we use the polar
+            # equation of an ellipse (wrt center) to find that point
+            # and then rotate that point about the ellipse center to
+            # correspond to the current orientation of the ellipse
+            xp, yp = ellipse_point(a, e,
+                                   (omega_rotate-omega_orbit)*t, omega_orbit*t)
 
-        # now we extend by the half-height of the person along the
-        # line connecting the person to the center of the ellipse,
-        # since we draw a person by specifying the middle of the
-        # person.
-        xp1 = xp + L/2*np.cos(omega_rotate*t)
-        yp1 = yp + L/2*np.sin(omega_rotate*t)
+            # now we extend by the half-height of the person along the
+            # line connecting the person to the center of the ellipse,
+            # since we draw a person by specifying the middle of the
+            # person.
+            xp1 = xp + L/2*np.cos(omega_rotate*t)
+            yp1 = yp + L/2*np.sin(omega_rotate*t)
         
-        center = (x_m + xp1, y_m + yp1)        
-        sf.draw_person(center, L, omega_rotate*t-np.pi/2, color="r")
+            center = (x_m + xp1, y_m + yp1)        
+            sf.draw_person(center, L, omega_rotate*t-np.pi/2, color="r")
 
-        # and a line to the person
-        # plt.plot([x_m, x_m + xp], [y_m, y_m + yp], "k", ls="--")
-
-        # and a line to the Sun and Moon
-        plt.plot([0, x_m], [0, y_m], "k", ls="--")
+            # and a line to the Sun and Moon
+            plt.plot([0, x_m], [0, y_m], "k", ls="--")
         
-        plt.axis("off")
+            plt.axis("off")
 
-        plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+            plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-        max_dist = R_orbit + R_moon + 1.5*L
-        plt.axis([-max_dist, max_dist, -max_dist, max_dist])
+            max_dist = R_orbit + R_moon + 1.5*L
+            plt.axis([-max_dist, max_dist, -max_dist, max_dist])
+
+            f = plt.gcf()
+            
+            if scene == "unlocked":
+                plt.text(0.5, 0.95,
+                         "no tidal locking",
+                         horizontalalignment="center",
+                         transform=f.transFigure)
+                plt.text(0.5, 0.05,
+                         "the moon's rotation is independent of its orbital period around the planet",
+                         horizontalalignment="center",
+                         transform=f.transFigure)
+                
+            else:
+                plt.text(0.5, 0.95,                
+                         "tidally locked moon",
+                         horizontalalignment="center",
+                         transform=f.transFigure)
+                plt.text(0.5, 0.05,
+                         "the moon's rotation period is equal to its orbital period",
+                         horizontalalignment="center",
+                         transform=f.transFigure)
+                
+            ax = plt.gca()
+            ax.set_aspect("equal", "datalim")
         
-        ax = plt.gca()
-        ax.set_aspect("equal", "datalim")
-        
-        f = plt.gcf()
-        f.set_size_inches(7.2,7.2)
+
+            f.set_size_inches(7.2,7.2)
     
-        plt.savefig("tidal_locking_{:04}.png".format(n))
+            plt.savefig("tidal_locking_{:04}.png".format(n+d*N))
         
 
 
