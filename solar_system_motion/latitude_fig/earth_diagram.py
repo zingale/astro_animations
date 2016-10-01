b476c46d1805755d008548e96c15a32bf22a6c83
Michael Zingale <michael.zingale@stonybrook.edu>
2016-10-01 17:12:40 -0400

    more progress

--- solar_system_motion/latitude_fig/earth_diagram.py
+++ solar_system_motion/latitude_fig/earth_diagram.py
@@ -13,7 +13,8 @@ class Earth(object):
 
         self.lat = latitude
 
-        # making it negative gives us the summer solstice
+        # note that angles are measured counterclockwise from the +x
+        # axis.  making it negative gives us the summer solstice
         self.tilt = axial_tilt
 
         # scalings for the person and earth
@@ -57,7 +58,7 @@ class Earth(object):
 
     def draw_rot_axis(self):
 
-        overhang = 1.1
+        overhang = 1.2
 
         ps = (self.x0, self.y0 - overhang*self.R)
         pe = (self.x0, self.y0 + overhang*self.R)
@@ -68,28 +69,52 @@ class Earth(object):
         plt.plot([ts[0], te[0]], [ts[1], te[1]], color="b")
 
 
-        a = arrow.ArcArrow((0, 0), 0.5*self.R, theta_start=90+self.tilt, theta_end=90.0)
+        a = arrow.ArcArrow((0, 0), 0.5*self.R, 
+                           theta_start=90+self.tilt, theta_end=90.0)
         a.draw(color="b")
 
         mid = 0.5*(90 + self.tilt + 90)
         plt.text(0.51*self.R*np.cos(np.radians(mid)),
-                 0.51*self.R*np.sin(np.radians(mid)), r"$\alpha$", color="b", horizontalalignment="left")
+                 0.51*self.R*np.sin(np.radians(mid)), 
+                 r"$\alpha$", color="b", horizontalalignment="left")
+
+
+    def draw_parallel(self, l, color="k", ls="-"):
+        """ draw a line of latitude """
+
+        # working in Earth coordinates
+        # find the (x, y) of the parallel start and end
+        xs = self.x0 - self.R*np.cos(np.radians(l))
+        ys = self.y0 + self.R*np.sin(np.radians(l))
+        
+        xe = self.x0 + self.R*np.cos(np.radians(l))
+        ye = self.y0 + self.R*np.sin(np.radians(l))
+        
+
+        # since we are tilted, the actual angle in the drawing
+        # (from +x) is l + tilt
+        angle = self.tilt
+
+        # transform the points into the rotated frame
+        ts = sf._rotate((xs, ys), (self.x0, self.y0), np.radians(angle))
+        te = sf._rotate((xe, ye), (self.x0, self.y0), np.radians(angle))
+
+        plt.plot([ts[0], te[0]], [ts[1], te[1]], color=color, ls=ls)
 
 
     def draw_equator(self):
+        self.draw_parallel(0, color="b")
 
-        ps = (self.x0, self.y0 - self.R)
-        pe = (self.x0, self.y0 + self.R)
+    def draw_tropics(self):
+        self.draw_parallel(np.abs(self.tilt), color="g", ls="--")
+        self.draw_parallel(-np.abs(self.tilt), color="g", ls="--")
 
-        ts = sf._rotate(ps, (self.x0, self.y0),
-                        np.radians(90+self.tilt))
-        te = sf._rotate(pe, (self.x0, self.y0),
-                        np.radians(90+self.tilt))
-
-        plt.plot([ts[0], te[0]], [ts[1], te[1]], color="b")
+    def draw_arctic_circles(self):
+        self.draw_parallel(90-np.abs(self.tilt), color="g", ls="--")
+        self.draw_parallel(-90+np.abs(self.tilt), color="g", ls="--")
 
 
-    def draw_latitude(self):
+    def draw_my_latitude(self):
 
         angle = self.lat + self.tilt
 
@@ -114,6 +139,26 @@ class Earth(object):
         plt.text(0.51*self.R*np.cos(np.radians(mid)),
                  0.51*self.R*np.sin(np.radians(mid)), r"$l$", color="r", horizontalalignment="left")
 
+
+    def draw_horizon(self):
+        angle = self.tilt + self.lat
+
+        sc = ( self.x0 + self.R*np.cos(np.radians(angle)),
+               self.y0 + self.R*np.sin(np.radians(angle)) )
+
+        ps = (-0.75*self.R, 0)
+        pe = (0.75*self.R, 0)
+
+        ts = sf._rotate(ps, (0, 0), np.radians(90+angle))
+        te = sf._rotate(pe, (0, 0), np.radians(90+angle))
+
+        plt.plot([sc[0]+ts[0], sc[0]+te[0]], [sc[1]+ts[1], sc[1]+te[1]], color="c")
+        plt.text(sc[0]+ts[0], sc[1]+ts[1], "local horizon", 
+                 horizontalalignment="left",
+                 verticalalignment="top",
+                 rotation=270+angle, color="c")             
+
+
     def draw_zenith(self):
         pass
 
@@ -127,7 +172,10 @@ def doit():
     e.draw_ecliptic()
     e.draw_rot_axis()
     e.draw_equator()
-    e.draw_latitude()
+    e.draw_tropics()
+    e.draw_arctic_circles()
+    e.draw_my_latitude()
+    e.draw_horizon()
 
     plt.axis("off")
 
@@ -137,6 +185,7 @@ def doit():
     f = plt.gcf()
     f.set_size_inches(12.8, 7.2)
 
+    plt.tight_layout()
     plt.savefig("test.png")
 
 
