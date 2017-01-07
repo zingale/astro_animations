bb257c1a509678eb0995495a55484d2ca820f531
Michael Zingale <michael.zingale@stonybrook.edu>
2017-01-06 19:29:15 -0500

    update style

--- binary_exoplanets/eclipsing_binaries/eclipsing_binary.py
+++ binary_exoplanets/eclipsing_binaries/eclipsing_binary.py
@@ -1,6 +1,5 @@
-import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 # compute the orbits of eclipsing binary stars, seen nearly edge-on.
 #
@@ -18,11 +17,10 @@ G = 6.67428e-8        # cm^3 g^{-1} s^{-2}
 M_sun = 1.98892e33    # g
 AU = 1.49598e13       # cm
 year = 3.1556926e7
-deg_to_rad = math.pi/180.0
 
 # a simple class to serve as a container for the orbital information
 # for the two stars
-class solarsystem:
+class SolarSystem(object):
     
     def __init__ (self, M_star1=1, M_star2=0.1):
 
@@ -31,19 +29,19 @@ class solarsystem:
 
         # star1 properties
         self.M_star1 = M_star1
-        self.x_star1 = numpy.zeros(self.maxpoints)
-        self.y_star1 = numpy.zeros(self.maxpoints)
-        self.vx_star1 = numpy.zeros(self.maxpoints)
-        self.vy_star1 = numpy.zeros(self.maxpoints)
+        self.x_star1 = np.zeros(self.maxpoints)
+        self.y_star1 = np.zeros(self.maxpoints)
+        self.vx_star1 = np.zeros(self.maxpoints)
+        self.vy_star1 = np.zeros(self.maxpoints)
 
         # star2 properties
         self.M_star2 = M_star2
-        self.x_star2 = numpy.zeros(self.maxpoints)
-        self.y_star2 = numpy.zeros(self.maxpoints)
-        self.vx_star2 = numpy.zeros(self.maxpoints)
-        self.vy_star2 = numpy.zeros(self.maxpoints)
+        self.x_star2 = np.zeros(self.maxpoints)
+        self.y_star2 = np.zeros(self.maxpoints)
+        self.vx_star2 = np.zeros(self.maxpoints)
+        self.vy_star2 = np.zeros(self.maxpoints)
 
-        self.t = numpy.zeros(self.maxpoints)
+        self.t = np.zeros(self.maxpoints)
 
 
 
@@ -68,7 +66,7 @@ def radial_velocity():
     inc = 88.75    # degrees
 
     # create the solar system container
-    ss = solarsystem(M_star1 = M_star1, M_star2 = M_star2)
+    ss = SolarSystem(M_star1 = M_star1, M_star2 = M_star2)
 
 
     # set the radii and temperatures -- dimensionless -- we are going
@@ -88,16 +86,16 @@ def radial_velocity():
     
     # we are going to put the center of mass at the origin and star 2
     # initially on the +x axis and the star 1 initially on the -x axis
-    x_star1_init = -a_star1*(1.0 - ecc)*math.cos(theta)
-    y_star1_init = -a_star1*(1.0 - ecc)*math.sin(theta)
+    x_star1_init = -a_star1*(1.0 - ecc)*np.cos(theta)
+    y_star1_init = -a_star1*(1.0 - ecc)*np.sin(theta)
 
-    x_star2_init = a_star2*(1.0 - ecc)*math.cos(theta)
-    y_star2_init = a_star2*(1.0 - ecc)*math.sin(theta)
+    x_star2_init = a_star2*(1.0 - ecc)*np.cos(theta)
+    y_star2_init = a_star2*(1.0 - ecc)*np.sin(theta)
 
 
     # Kepler's laws should tell us the orbital period
     # P^2 = 4 pi^2 (a_star1 + a_star2)^3 / (G (M_star1 + M_star2))
-    period = math.sqrt(4*math.pi**2*(a_star1 + a_star2)**3/(G*(M_star1 + M_star2)))
+    period = np.sqrt(4*np.pi**2*(a_star1 + a_star2)**3/(G*(M_star1 + M_star2)))
 
     print "period = ", period/year
 
@@ -106,16 +104,16 @@ def radial_velocity():
 
     # first compute the velocity of the reduced mass at perihelion
     # (C&O Eq. 2.33)
-    v_mu = math.sqrt( (G*(M_star1 + M_star2)/(a_star1 + a_star2)) *
-                      (1.0 + ecc)/(1.0 - ecc) )
+    v_mu = np.sqrt( (G*(M_star1 + M_star2)/(a_star1 + a_star2)) *
+                    (1.0 + ecc)/(1.0 - ecc) )
 
     # then v_star2 = (mu/m_star2)*v_mu
-    vx_star2_init = -(M_star1/(M_star1 + M_star2))*v_mu*math.sin(theta)
-    vy_star2_init = (M_star1/(M_star1 + M_star2))*v_mu*math.cos(theta)
+    vx_star2_init = -(M_star1/(M_star1 + M_star2))*v_mu*np.sin(theta)
+    vy_star2_init = (M_star1/(M_star1 + M_star2))*v_mu*np.cos(theta)
 
     # then v_star1 = (mu/m_star1)*v_mu
-    vx_star1_init = (M_star2/(M_star1 + M_star2))*v_mu*math.sin(theta)
-    vy_star1_init = -(M_star2/(M_star1 + M_star2))*v_mu*math.cos(theta)
+    vx_star1_init = (M_star2/(M_star1 + M_star2))*v_mu*np.sin(theta)
+    vy_star1_init = -(M_star2/(M_star1 + M_star2))*v_mu*np.cos(theta)
 
     
     # set the timestep in terms of the orbital period
@@ -130,8 +128,8 @@ def radial_velocity():
 
     # apply the projection to account for the inclination wrt the
     # observer
-    ss.y_star1[0:ss.npts] = ss.y_star1[0:ss.npts]*math.cos(inc*deg_to_rad)
-    ss.y_star2[0:ss.npts] = ss.y_star2[0:ss.npts]*math.cos(inc*deg_to_rad)
+    ss.y_star1[0:ss.npts] = ss.y_star1[0:ss.npts]*np.cos(np.radians(inc))
+    ss.y_star2[0:ss.npts] = ss.y_star2[0:ss.npts]*np.cos(np.radians(inc))
 
     # we will keep one star fixed and draw the relative `orbit' of the
     # other
@@ -142,9 +140,9 @@ def radial_velocity():
     # cut angle -- we don't want to plot the orbit where the stationary
     # star is (origin), so specify the angle wrt the +y axis where we
     # don't plot
-    cut_angle = 10*deg_to_rad
+    cut_angle = np.radians(10)
 
-    frac = cut_angle/(2.0*math.pi)
+    frac = cut_angle/(2.0*np.pi)
 
     # star 2 starts on the -x axis.  3/4 through the orbit, it will
     # be behind the star 1.  Compute the range of steps to skip plotting
@@ -188,63 +186,58 @@ def radial_velocity():
     # t_h = star 2 fully emerged from behind star 1
     t_a = t_b = t_c = t_d = t_e = t_f = t_g = t_h = -1.0
 
-    n = 0
-    while (n < ss.npts):
+    for n in range(ss.npts):
 
-        if (y_rel[n] <= 0):
+        if y_rel[n] <= 0:
             
             # star 2 in front of star 1
-            if (x_rel[n] + R2*rad_scal > -R1*rad_scal and t_a == -1):
+            if x_rel[n] + R2*rad_scal > -R1*rad_scal and t_a == -1:
                 t_a = ss.t[n]
 
-            if (x_rel[n] - R2*rad_scal >= -R1*rad_scal and t_b == -1):
+            if x_rel[n] - R2*rad_scal >= -R1*rad_scal and t_b == -1:
                 t_b = ss.t[n]
 
-            if (x_rel[n] + R2*rad_scal > R1*rad_scal and t_c == -1):
+            if x_rel[n] + R2*rad_scal > R1*rad_scal and t_c == -1:
                 t_c = ss.t[n]
 
-            if (x_rel[n] - R2*rad_scal >= R1*rad_scal and t_d == -1):
+            if x_rel[n] - R2*rad_scal >= R1*rad_scal and t_d == -1:
                 t_d = ss.t[n]
 
         else:
 
             # star 2 behind star 1
-            if (x_rel[n] - R2*rad_scal < R1*rad_scal and t_e == -1):
+            if x_rel[n] - R2*rad_scal < R1*rad_scal and t_e == -1:
                 t_e = ss.t[n]
 
-            if (x_rel[n] + R2*rad_scal <= R1*rad_scal and t_f == -1):
+            if x_rel[n] + R2*rad_scal <= R1*rad_scal and t_f == -1:
                 t_f = ss.t[n]
 
-            if (x_rel[n] - R2*rad_scal < -R1*rad_scal and t_g == -1):
+            if x_rel[n] - R2*rad_scal < -R1*rad_scal and t_g == -1:
                 t_g = ss.t[n]
 
-            if (x_rel[n] + R2*rad_scal <= -R1*rad_scal and t_h == -1):
+            if x_rel[n] + R2*rad_scal <= -R1*rad_scal and t_h == -1:
                 t_h = ss.t[n]
             
 
-        n += 1
-
-
     # make an array of the flux vs. time -- this is the light curve
-    f_system = numpy.zeros(ss.npts, numpy.float64)
+    f_system = np.zeros(ss.npts, np.float64)
 
-    n = 0
-    while (n < ss.npts):
+    for n in range(ss.npts):
 
         f_system[n] = 1.0
         
         # star 2 passing in front of star 1
 
-        if (ss.t[n] >= t_a and ss.t[n] < t_b):
+        if ss.t[n] >= t_a and ss.t[n] < t_b:
             # linearly interpolate between f = 1 at t = t_a and
             # f = f_star2_transit at t = t_b
             slope = (f_star2_transit - 1.0)/(t_b - t_a)
             f_system[n] = slope*(ss.t[n] - t_b) + f_star2_transit
 
-        elif (ss.t[n] >= t_b and ss.t[n] < t_c):
+        elif ss.t[n] >= t_b and ss.t[n] < t_c:
             f_system[n] = f_star2_transit
 
-        elif (ss.t[n] >= t_c and ss.t[n] < t_d):
+        elif ss.t[n] >= t_c and ss.t[n] < t_d:
             # linearly interpolate between f = f_star2_transit at 
             # t = t_c and f = 1 at t = t_d
             slope = (1.0 - f_star2_transit)/(t_d - t_c)
@@ -252,23 +245,20 @@ def radial_velocity():
 
         # star 2 passing behind star 1
 
-        elif (ss.t[n] >= t_e and ss.t[n] < t_f):
+        elif ss.t[n] >= t_e and ss.t[n] < t_f:
             # linearly interpolate between f = 1 at t = t_e and
             # f = f_star2_blocked at t = t_f
             slope = (f_star2_blocked - 1.0)/(t_f - t_e)
             f_system[n] = slope*(ss.t[n] - t_f) + f_star2_blocked
 
-        elif (ss.t[n] >= t_f and ss.t[n] < t_g):
+        elif ss.t[n] >= t_f and ss.t[n] < t_g:
             f_system[n] = f_star2_blocked
 
-        elif (ss.t[n] >= t_g and ss.t[n] < t_h):
+        elif ss.t[n] >= t_g and ss.t[n] < t_h:
             # linearly interpolate between f = f_star2_blocked 
             # at t = t_g and f = 1 at t = t_h
             slope = (1.0 - f_star2_blocked)/(t_h - t_g)
             f_system[n] = slope*(ss.t[n] - t_h) + 1.0
-
-        n += 1
-
 
     # ================================================================
     # plotting
@@ -277,92 +267,88 @@ def radial_velocity():
 
     iframe = 0
 
-    n = 0
-    while (n < ss.npts):
+    for n in range(ss.npts):
 
-        pylab.clf()
+        plt.clf()
 
-        pylab.subplots_adjust(left=0.1,right=0.9,bottom=0.1,top=0.9)
+        plt.subplots_adjust(left=0.1,right=0.9,bottom=0.1,top=0.9)
 
-        pylab.subplot(211)
+        plt.subplot(211)
 
-        a = pylab.gca()
+        a = plt.gca()
         a.set_aspect("equal", "datalim")
-        pylab.axis("off")
+        plt.axis("off")
 
-        if (y_rel[n] < 0.0):
+        if y_rel[n] < 0.0:
             # star 2 is in front of star 1
 
             # plot star 1's orbit position -- set to be the origin
             xc1, yc1 = circle(0, 0, R1*rad_scal)
-            pylab.fill(xc1, yc1, 'r', ec="none")
+            plt.fill(xc1, yc1, 'r', ec="none")
 
             # plot star 2's relative orbit and position
             xc2, yc2 = circle(x_rel[n], y_rel[n], R2*rad_scal)
-            pylab.fill(xc2, yc2, 'b', ec="none")
+            plt.fill(xc2, yc2, 'b', ec="none")
 
         else:
             # star 1 is in front of star 2
 
             # plot star 2's relative orbit and position
             xc2, yc2 = circle(x_rel[n], y_rel[n], R2*rad_scal)
-            pylab.fill(xc2, yc2, 'b', ec="none")
+            plt.fill(xc2, yc2, 'b', ec="none")
 
             # plot star 1's orbit position -- set to be the origin
             xc1, yc1 = circle(0, 0, R1*rad_scal)
-            pylab.fill(xc1, yc1, 'r', ec="none")
+            plt.fill(xc1, yc1, 'r', ec="none")
 
 
 
         # plot the orbit -- in two segment
-        pylab.plot(x_rel[0:cut_index1],y_rel[0:cut_index1], 
+        plt.plot(x_rel[0:cut_index1],y_rel[0:cut_index1], 
                    color="0.75", linestyle="--",alpha=0.5)
-        pylab.plot(x_rel[cut_index2:ss.npts],y_rel[cut_index2:ss.npts], 
+        plt.plot(x_rel[cut_index2:ss.npts],y_rel[cut_index2:ss.npts], 
                    color="0.75", linestyle="--",alpha=0.5)
 
 
-        pylab.text(-1.5*a_star2, 1.1*a_star2, "star 1: T = %5.0f K" % (T1), 
+        plt.text(-1.5*a_star2, 1.1*a_star2, "star 1: T = %5.0f K" % (T1), 
                     color="r", fontsize=10)
-        pylab.text(-1.5*a_star2, 0.95*a_star2, "star 2: T = %5.0f K" % (T2), 
+        plt.text(-1.5*a_star2, 0.95*a_star2, "star 2: T = %5.0f K" % (T2), 
                     color="b", fontsize=10)
-        pylab.text(-1.5*a_star2, 0.8*a_star2, 
+        plt.text(-1.5*a_star2, 0.8*a_star2, 
                     "R (star 1) / R (star 2) = %4.1f" % (R1/R2), 
                     color="k", fontsize=10)
 
-        pylab.axis([-1.5*a_star2,1.5*a_star2,-1.25*a_star2,1.25*a_star2])
+        plt.axis([-1.5*a_star2,1.5*a_star2,-1.25*a_star2,1.25*a_star2])
 
 
-        pylab.subplot(212)
+        plt.subplot(212)
 
-        pylab.plot(ss.t[0:ss.npts]/tmax, f_system, "k")
-        pylab.scatter([ss.t[n]/tmax], [f_system[n]], color="k")
+        plt.plot(ss.t[0:ss.npts]/tmax, f_system, "k")
+        plt.scatter([ss.t[n]/tmax], [f_system[n]], color="k")
 
-        pylab.xlim(0.0,1.0)
-        pylab.ylim(0.0,1.1)
+        plt.xlim(0.0,1.0)
+        plt.ylim(0.0,1.1)
     
-        pylab.xlabel("t/P")
-        pylab.ylabel("relative flux")
+        plt.xlabel("t/P")
+        plt.ylabel("relative flux")
 
 
-        f = pylab.gcf()
-        f.set_size_inches(5.0,6.5)
+        f = plt.gcf()
+        f.set_size_inches(6.0,7.2)
   
-        outfile = "eclipsingbinar_%04d.png" % iframe
-        pylab.savefig(outfile)
+        outfile = "eclipsingbinar_{:04d}.png".format(iframe)
+        plt.savefig(outfile)
 
 
         iframe += 1
-        n += 1
-
-
 
 
 def circle(xc, yc, r):
     
-    theta = numpy.arange(361)*2.0*math.pi/360
+    theta = np.arange(361)*2.0*np.pi/360
 
-    x = r*numpy.cos(theta) + xc
-    y = r*numpy.sin(theta) + yc
+    x = r*np.cos(theta) + xc
+    y = r*np.sin(theta) + yc
 
     return x, y
 
@@ -376,13 +362,13 @@ def integrate_system(ss,
 
     # allocate storage for R-K intermediate results
     # y[0:3] will hold the star info, y[4:7] will hold the planet info
-    k1 = numpy.zeros(8, numpy.float64)
-    k2 = numpy.zeros(8, numpy.float64)
-    k3 = numpy.zeros(8, numpy.float64)
-    k4 = numpy.zeros(8, numpy.float64)
+    k1 = np.zeros(8, np.float64)
+    k2 = np.zeros(8, np.float64)
+    k3 = np.zeros(8, np.float64)
+    k4 = np.zeros(8, np.float64)
 
-    y = numpy.zeros(8, numpy.float64)
-    f = numpy.zeros(8, numpy.float64)
+    y = np.zeros(8, np.float64)
+    f = np.zeros(8, np.float64)
 
 
 
@@ -419,7 +405,7 @@ def integrate_system(ss,
     ss.t[0] = t
 
     n = 1
-    while (n < ss.maxpoints and t < tmax):
+    while n < ss.maxpoints and t < tmax:
 
         f = rhs(t, y, ss.M_star1, ss.M_star2)
         k1[:] = dt*f[:]
@@ -461,7 +447,7 @@ def integrate_system(ss,
 
 def rhs(t,y,M_star1,M_star2):
 
-    f = numpy.zeros(8, numpy.float64)
+    f = np.zeros(8, np.float64)
 
     # y[0] = x_star1, y[1] = y_star1, y[2] = vx_star1, y[3] = vy_star1
     # y[4] = x_star2,    y[5] = y_star2,    y[6] = vx_star2,    y[7] = vy_star2
@@ -481,7 +467,7 @@ def rhs(t,y,M_star1,M_star2):
 
 
     # distance between star and planet
-    r = numpy.sqrt((x_star2 - x_star1)**2 + (y_star2 - y_star1)**2)
+    r = np.sqrt((x_star2 - x_star1)**2 + (y_star2 - y_star1)**2)
 
 
     f[0] = vx_star1  # d(x_star1) / dt
