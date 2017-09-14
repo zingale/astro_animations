efede06646c8146e6dcb046eef221020e3505212
Michael Zingale <michael.zingale@stonybrook.edu>
2017-09-14 12:37:23 -0400

    update style

--- ellipses/ellipse_fig/ellipse_vel.py
+++ ellipses/ellipse_fig/ellipse_vel.py
@@ -1,6 +1,5 @@
-import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 # show the velocity vectors at perihelion and aphelion for ellipse geometry
 
@@ -11,61 +10,56 @@ def ellipse():
 
     # theta ranges from 0 to 2pi
     npts = 250
-    theta = numpy.arange(npts)*2.0*math.pi/(npts-1)
+    theta = np.arange(npts)*2.0*np.pi/(npts-1)
 
     e = 0.5
     a = 1.0
-    b = a*math.sqrt(1-e*e)
+    b = a*np.sqrt(1-e*e)
 
     n = npts/5
- 
-    r = a*(1.0 - e*e)/(1.0 + e*numpy.cos(theta))
 
-    x = r*numpy.cos(theta)
-    y = r*numpy.sin(theta)
+    r = a*(1.0 - e*e)/(1.0 + e*np.cos(theta))
 
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
 
     # plotting
-    pylab.clf()
+    plt.clf()
 
-    ax = pylab.gca()
+    ax = plt.gca()
     ax.set_aspect("equal", "datalim")
-    pylab.axis("off")
-        
+    plt.axis("off")
+
     # Draw the ellipse
-    pylab.plot(x,y,color="b")
+    plt.plot(x,y,color="b")
 
     # primary foci
-    pylab.scatter([0],       [0], color="k", marker="x", s=200)
+    plt.scatter([0], [0], color="k", marker="x", s=200)
 
     # draw arrow to perihelion
     eps = 0.033*a
-    pylab.plot([0,a*(1-e)],[0,0], color='r')
-    pylab.plot([a*(1-e),a*(1-e)-eps],[0,eps], color='r')
-    pylab.plot([a*(1-e),a*(1-e)-eps],[0,-eps], color='r')
-    pylab.text(0.5*a*(1-e), -0.15*b, "r", color='r')
+    plt.plot([0,a*(1-e)],[0,0], color='r')
+    plt.plot([a*(1-e),a*(1-e)-eps],[0,eps], color='r')
+    plt.plot([a*(1-e),a*(1-e)-eps],[0,-eps], color='r')
+    plt.text(0.5*a*(1-e), -0.15*b, "r", color='r')
 
-    
+
     # draw velocity at perihelion
     vel = 0.66*a
-    pylab.plot([a*(1-e),a*(1-e)], [0, vel], color='g')
-    pylab.plot([a*(1-e),a*(1-e)-eps], [vel, vel-eps], color='g')
-    pylab.plot([a*(1-e),a*(1-e)+eps], [vel, vel-eps], color='g')
-    pylab.text(a*(1-e)+0.05*b,0.5*vel, "v", color='g')
-    
-    pylab.axis([-1.75,.75,-1.25,1.25])
- 
+    plt.plot([a*(1-e),a*(1-e)], [0, vel], color='g')
+    plt.plot([a*(1-e),a*(1-e)-eps], [vel, vel-eps], color='g')
+    plt.plot([a*(1-e),a*(1-e)+eps], [vel, vel-eps], color='g')
+    plt.text(a*(1-e)+0.05*b,0.5*vel, "v", color='g')
 
-    f = pylab.gcf()
+    plt.axis([-1.75,.75,-1.25,1.25])
+
+
+    f = plt.gcf()
     f.set_size_inches(6.0,6.0)
 
     outfile = "ellipse_vel.png"
-    pylab.savefig(outfile)
+    plt.savefig(outfile)
 
 
 if __name__== "__main__":
     ellipse()
-
-
-    
-        
