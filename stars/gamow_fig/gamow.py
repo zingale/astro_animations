feea8c76fcc6e2834898dbd97d0653829edad573
Michael Zingale <michael.zingale@stonybrook.edu>
2018-09-16 12:21:49 -0400

    some updates to show structure

--- stars/gamow_fig/gamow.py
+++ stars/gamow_fig/gamow.py
@@ -32,6 +32,11 @@ def gamow(E, T):
     # E should be in keV
     return np.exp(-E/(k_keV*T) - b/np.sqrt(E))
 
+def max_boltz(E, T):
+    return np.exp(-E/(k_keV*T))
+
+def tunnel(E, T):
+    return np.exp(- b/np.sqrt(E))
 
 def gaussian_fit(E, T):
     # this is Clayton Eqs. 4-48 to 4-51
@@ -49,17 +54,24 @@ print("E_0 = ", E_0)
 E = np.linspace(0, 100, 200)
 
 
-plt.plot(E, gamow(E,T), label=r"$e^{-E/kT - bE^{-1/2}}$")
-plt.plot(E, gaussian_fit(E,T), color="r", ls=":", label=r"$e^{-\tau} \cdot e^{-[(E-E_0)/(\Delta/2)]^2}$")
+plt.plot(E, 1.e15*gamow(E,T), label=r"$e^{-E/kT - bE^{-1/2}} (\times 10^{15})$")
+plt.plot(E, max_boltz(E,T), label=r"Maxwell-Boltzmann tail")
+plt.plot(E, 1.e7*tunnel(E,T), label=r"tunneling factor $(\times 10^7)$")
+#plt.plot(E, gaussian_fit(E,T), color="r", ls=":", label=r"$e^{-\tau} \cdot e^{-[(E-E_0)/(\Delta/2)]^2}$")
+
+plt.plot([E_0, E_0], [0, 1], ls=":", color="0.5")
 
 plt.legend(frameon=False, fontsize=12)
 
 ax = plt.gca()
+ax.set_xlim(20, 60)
+ax.set_ylim(0,1.e-4)
 ax.yaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
 
 
 plt.xlabel("E (keV)")
-plt.title(r"Gamow peak + approximate Gaussian: $^{12}\mathrm{C}(p,\gamma){^{13}\mathrm{N}},\ T = 3\times 10^7~\mathrm{K}$", fontsize=12)
+#plt.title(r"Gamow peak + approximate Gaussian: $^{12}\mathrm{C}(p,\gamma){^{13}\mathrm{N}},\ T = 3\times 10^7~\mathrm{K}$", fontsize=12)
+plt.title(r"Gamow peak: $^{12}\mathrm{C}(p,\gamma){^{13}\mathrm{N}},\ T = 3\times 10^7~\mathrm{K}$", fontsize=12)
 
 plt.tight_layout()
 
