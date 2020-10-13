3585bae6afb66c9b98a3dd3cfe186bb81e47461d
Michael Zingale <michael.zingale@stonybrook.edu>
2020-10-13 10:13:42 -0400

    clean up

--- stars/HR_fig/HR_radius.py
+++ stars/HR_fig/HR_radius.py
@@ -7,9 +7,8 @@
 # M. Zingale (2010-12-13)
 
 
-import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 def HR_radius():
 
@@ -36,16 +35,16 @@ def HR_radius():
     #-------------------------------------------------------------------------
     # main sequence data taken from Carroll & Ostlie, Appendix G
     nstars = 11
-    M = numpy.zeros(nstars, numpy.float64)
-    T = numpy.zeros(nstars, numpy.float64)
-    R = numpy.zeros(nstars, numpy.float64)
-    L = numpy.zeros(nstars, numpy.float64)
-    
+    M = np.zeros(nstars, np.float64)
+    T = np.zeros(nstars, np.float64)
+    R = np.zeros(nstars, np.float64)
+    L = np.zeros(nstars, np.float64)
+
     # spectral type
-    spectralTypes = ['O8', 'B0', 'B3', 'B5', 'A0', 'A5', 'F5', 'Sun', 'K5', 'M0', 'M5']
+    spectral_types = ['O8', 'B0', 'B3', 'B5', 'A0', 'A5', 'F5', 'Sun', 'K5', 'M0', 'M5']
 
     # mass (solar masses)
-    M[:] = [23, 17.5, 7.6, 5.9, 2.9, 1.8, 1.2, 1,  0.67, 0.51, 0.21]
+    M[:] = [23, 17.5, 7.6, 5.9, 2.9, 1.8, 1.2, 1, 0.67, 0.51, 0.21]
 
     # temperature (K)
     T[:] = [35800, 32500, 18800, 15200, 9800, 8190, 6650, T_sun, 4410, 3840, 3170]
@@ -59,109 +58,97 @@ def HR_radius():
     #-------------------------------------------------------------------------
 
 
-    ax = pylab.subplot(111)
+    ax = plt.subplot(111)
     ax.set_xscale('log')
     ax.set_yscale('log')
-    
+
     # draw and label the main sequence
-    pylab.scatter(T,L, s=100, marker="+", color="r", lw=2)
-    pylab.plot(T,L,'b-')
-    n = 0
-    while (n < len(spectralTypes)):
+    plt.scatter(T, L, s=100, marker="+", color="r", lw=2)
+    plt.plot(T, L, 'b-')
+    for mstar, tstar, lstar, spec_star in zip(M, T, L, spectral_types):
 
-        if (label_masses):
-            if (M[n] >= 1.0):
-                pylab.text(1.05*T[n], L[n], "%4.1f $M_\odot$" % (M[n]),fontsize=10,
-                           horizontalalignment="right", verticalalignment="center", color="r")
+        if label_masses:
+            if mstar >= 1.0:
+                plt.text(1.05*tstar, lstar, fr"{mstar:4.1f} $M_\odot$", fontsize=10,
+                         horizontalalignment="right", verticalalignment="center", color="r")
             else:
-                pylab.text(1.05*T[n], L[n], "%3.2f $M_\odot$" % (M[n]),fontsize=10, 
-                           horizontalalignment="right", verticalalignment="center", color="r")
+                plt.text(1.05*tstar, lstar, fr"{mstar:3.2f} $M_\odot$", fontsize=10,
+                         horizontalalignment="right", verticalalignment="center", color="r")
+
+        plt.text(0.9*tstar, lstar, spec_star)
 
 
-        pylab.text(0.9*T[n], L[n], spectralTypes[n])
-
-        n += 1
-    
-
-    if (draw_radii):
+    if draw_radii:
 
         # draw in lines of constant radius
         Rvals = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000, 10000]
 
         nTpts = 25
-        dlogT = (math.log10(Tmax) - math.log10(Tmin))/(nTpts-1)
-        
-        Tplot = 10.0**(numpy.arange(nTpts)*dlogT + math.log10(Tmin))
-        
-        n = 0
-        while (n < len(Rvals)):
-    
+        dlogT = (np.log10(Tmax) - np.log10(Tmin))/(nTpts-1)
+
+        Tplot = 10.0**(np.arange(nTpts)*dlogT + np.log10(Tmin))
+
+        for n in range(len(Rvals)):
 
             # L/L_sun = (R/R_sun)**2 (T/T_sun)**4
             # compute L in L_sun units
             L = Rvals[n]**2 * (Tplot/T_sun)**4
 
-            pylab.plot(Tplot, L, linestyle='--', color="0.5")
-        
+            plt.plot(Tplot, L, linestyle='--', color="0.5")
+
             # draw in labels -- here we space the T position of the labels equally
-            # in log, by empericallly picking T = 40000 for L = 1.e-4 and 
+            # in log, by empericallly picking T = 40000 for L = 1.e-4 and
             # T = 3000 for L = 1.e4, and doing a line in log-space
-            slope = (math.log10(40000) - math.log10(3000))/(math.log10(1.e-4) - math.log10(1.e4))
-            xpos = math.log10(40000) + slope*(math.log10(Rvals[n]) - math.log10(1.e-4))
+            slope = (np.log10(40000) - np.log10(3000))/(np.log10(1.e-4) - np.log10(1.e4))
+            xpos = np.log10(40000) + slope*(np.log10(Rvals[n]) - np.log10(1.e-4))
             xpos = 10.0**xpos
 
             ypos = Rvals[n]**2 * (xpos/T_sun)**4
 
 
             if (xpos > Tmin and xpos < Tmax and ypos > Lmin and ypos < Lmax):
-                pylab.text(xpos, ypos, "%g $R_\odot$" % (Rvals[n]), color="0.5")
+                plt.text(xpos, ypos, fr"{Rvals[n]:g} $R_\odot$", color="0.5")
 
-            n += 1
-
-
-    if (draw_WDs):
+    if draw_WDs:
 
         # draw a box along the R = 0.01 R_sun line to indicate the approximate
         # position of the white dwarfs
         L3 = 0.01**2 * (30000./T_sun)**4
         L75 = 0.01**2 * (7500./T_sun)**4
-        pylab.fill([30000,30000, 7500, 7500], [1.5*L3, 0.66*L3, 0.66*L75, 1.5*L75], 
-                   alpha=0.20, facecolor="b")
+        plt.fill([30000, 30000, 7500, 7500], [1.5*L3, 0.66*L3, 0.66*L75, 1.5*L75],
+                 alpha=0.20, facecolor="b")
 
         L15 = 0.01**2 * (15000./T_sun)**4
-        pylab.text(15000, L15, "white dwarfs", color="b", horizontalalignment="center")
+        plt.text(15000, L15, "white dwarfs", color="b", horizontalalignment="center")
 
     # draw x-axis (temperature) labels in a few spots
     Tplot = [40000, 20000, 10000, 5000, 2500]
     Tlabels = []
-    n = 0
-    while (n < len(Tplot)):
+
+    for n in range(len(Tplot)):
         Tlabels.append("%5.0f" % (Tplot[n]))
-        n += 1
-    
-    locs, labels = pylab.xticks(Tplot, Tlabels)
+
+    locs, labels = plt.xticks(Tplot, Tlabels)
 
     # reverse the x-axis
-    pylab.axis([Tmin, Tmax, Lmin, Lmax])
-    ax = pylab.gca()
+    plt.axis([Tmin, Tmax, Lmin, Lmax])
+    ax = plt.gca()
     ax.set_xlim(ax.get_xlim()[::-1])
 
     # turn off minor ticks
-    minorLocator = pylab.NullLocator()
+    minorLocator = plt.NullLocator()
     ax.xaxis.set_minor_locator(minorLocator)
 
-    pylab.xlabel("$T$ (K)")
-    pylab.ylabel(r"$L/L_\odot$")
+    plt.xlabel("$T$ (K)")
+    plt.ylabel(r"$L/L_\odot$")
 
-    pylab.subplots_adjust(left=0.125,right=0.95,bottom=0.1,top=0.95)
+    plt.tight_layout()
 
-    f = pylab.gcf()
-    f.set_size_inches(6.0,7.0)
+    f = plt.gcf()
+    f.set_size_inches(6.0, 7.0)
 
-    pylab.savefig("HR_radius.png")
+    plt.savefig("HR_radius.png")
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     HR_radius()
-
