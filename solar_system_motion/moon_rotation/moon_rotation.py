e27e4d247abf62d474ee5815195e2890306de77f
Michael Zingale <michael.zingale@stonybrook.edu>
2024-09-29 12:34:12 -0400

    update style

--- solar_system_motion/moon_rotation/moon_rotation.py
+++ solar_system_motion/moon_rotation/moon_rotation.py
@@ -1,8 +1,7 @@
 #!/bin/env python
 
-import math
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 # demonstrate synchronous rotation of the Moon
 
@@ -109,74 +108,69 @@ def doit():
 
     # compute the period of the orbit from Kepler's law and make
     # the timestep by 1/720th of a period
-    P_orbital = math.sqrt(4*math.pi*math.pi*a**3/(G*M_E))
+    P_orbital = np.sqrt(4*np.pi*np.pi*a**3/(G*M_E))
 
     # set the rotation period
     P_rotation = P_orbital
 
-    omega = 2*math.pi/P_rotation
+    omega = 2*np.pi/P_rotation
 
     # set the initial coordinates -- perihelion
     x_init = 0.0
     y_init = -a*(1.0 - e)
 
     # set the initial velocity
-    vx_init = math.sqrt( (G*M_E/a) * (1 + e) / (1 - e))
+    vx_init = np.sqrt( (G*M_E/a) * (1 + e) / (1 - e))
     vy_init = 0.0
-
 
     # the circular orbit will be our baseline
     orbit = trajectory()
 
-
-    print "period = ", P_orbital/day
+    print("period = ", P_orbital/day)
 
     dt = P_orbital/720.0
     tmax = 2*P_orbital
 
     orbit.integrate(x_init, y_init, vx_init, vy_init, dt, tmax)
 
-
     # plotting
 
     for n in range(orbit.npts):
 
-        plt.clf()
+        fig, ax = plt.subplots()
 
         # plot the Earth
-        plt.scatter([0], [0], s=650, color="k")
-        plt.scatter([0], [0], s=600, color="b")
+        ax.scatter([0], [0], s=650, color="k")
+        ax.scatter([0], [0], s=600, color="b")
 
         # plot the orbit
-        plt.plot(orbit.x[0:orbit.npts], orbit.y[0:orbit.npts], color="0.5", ls=":", lw=2)
+        ax.plot(orbit.x[0:orbit.npts], orbit.y[0:orbit.npts],
+                color="0.5", ls=":", lw=2)
 
         # plot moon (use zorder to put this on top of the orbit line)
         theta = np.arange(180)
         r = 0.05*d  # exaggerate the moon's size
         x_surface = orbit.x[n] + r*np.cos(theta)
         y_surface = orbit.y[n] + r*np.sin(theta)
-        plt.fill(x_surface,y_surface,"0.75", edgecolor="0.75", alpha=1.0, zorder=1000)
+        ax.fill(x_surface, y_surface, "0.75",
+                edgecolor="0.75", alpha=1.0, zorder=1000)
 
         # plot a point on the moon's surface
-        xpt = orbit.x[n] + r*np.cos(omega*orbit.t[n]+math.pi/2.0)
-        ypt = orbit.y[n] + r*np.sin(omega*orbit.t[n]+math.pi/2.0)
-        plt.scatter([xpt],[ypt],s=25,color="k")
+        xpt = orbit.x[n] + r*np.cos(omega*orbit.t[n]+np.pi/2.0)
+        ypt = orbit.y[n] + r*np.sin(omega*orbit.t[n]+np.pi/2.0)
+        ax.scatter([xpt], [ypt], s=25, color="k")
 
-
-        plt.axis([-1.1*d,1.1*d,-1.1*d,1.1*d])
-        plt.axis("off")
+        ax.axis([-1.1*d, 1.1*d, -1.1*d, 1.1*d])
+        ax.axis("off")
 
         plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-        ax = plt.gca()
         ax.set_aspect("equal", "datalim")
 
+        fig.set_size_inches(7.2,7.2)
 
-        f = plt.gcf()
-        f.set_size_inches(7.2,7.2)
+        fig.savefig(f"moon_rotation_{n:04d}.png")
+        plt.close(fig)
 
-        plt.savefig("moon_rotation_%04d.png" % n)
-
-
-if __name__== "__main__":
+if __name__ == "__main__":
     doit()
