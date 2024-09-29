1ed7ff5ada0370fc7085e394749c6cbe276d8a80
Michael Zingale <michael.zingale@stonybrook.edu>
2024-09-29 17:11:47 -0400

    update redblack plotting

--- CFD/red_black/red_black.py
+++ CFD/red_black/red_black.py
@@ -1,23 +1,23 @@
-import math
-import numpy
-import pylab
-import random
+import matplotlib.pyplot as plt
 
-# red black gauss seidel pattern
+"""
+red black gauss seidel pattern
+"""
 
-class marker:
+
+class Marker:
 
     def __init__(self, xc, yc, color):
 
         # a marker is indicated by its center (xc,yc).
         self.xc = xc
         self.yc = yc
-        
+
         # keep track of the color
         self.color = color
 
-        
-def RB():
+
+def red_black():
 
     # define the number of markers in x and y
     nx = 10
@@ -28,72 +28,51 @@ def RB():
 
     # create a list of marker objects, one at each grid location
     markers = []
-    
 
-    color = 0
-    while (color <= 1):
+    for color in [0, 1]:
+        for j in range(ny):
 
-        j = 0
-        while (j < ny):
-
-            if (color == 0):
+            if color == 0:
                 ioff = j % 2
             else:
                 ioff = 1 - (j % 2)
-                
-            i = ioff
-            while (i < nx):
-                markers.append(marker(i, j, color))
-                i += 2
 
-            j += 1
+            for i in range(ioff, nx, 2):
+                markers.append(Marker(i, j, color))
 
-        color += 1
-
-
-    pylab.clf()
+    fig, ax = plt.subplots()
 
     # the margins are funny -- we pick them to ensure that the
     # plot size is an integer multiple of the number of markers in
     # each dimension
-    pylab.subplots_adjust(left=0.1,  right=0.9,
-                          bottom=0.1,top=0.9)
+    plt.subplots_adjust(left=0.1, right=0.9,
+                        bottom=0.1, top=0.9)
 
     # draw the current state
-    n = 0
-    while (n < len(markers)):
+    for m in markers:
 
-        if (markers[n].color == 1):
+        if (m.color == 1):
             c = "r"
         else:
             c = "k"
 
-        pylab.fill([markers[n].xc-L/2, markers[n].xc-L/2,
-                    markers[n].xc+L/2, markers[n].xc+L/2,
-                    markers[n].xc-L/2],
-                   [markers[n].yc-L/2, markers[n].yc+L/2,
-                    markers[n].yc+L/2, markers[n].yc-L/2,
-                    markers[n].yc-L/2], 
-                   c)
+        ax.fill([m.xc-L/2, m.xc-L/2,
+                 m.xc+L/2, m.xc+L/2,
+                 m.xc-L/2],
+                [m.yc-L/2, m.yc+L/2,
+                 m.yc+L/2, m.yc-L/2,
+                 m.yc-L/2],
+                color=c)
 
-        n += 1
+    ax.axis([-0.5, nx-0.5, -0.5, ny-0.5])
+    ax.axis("off")
 
-            
-    ax = pylab.axis([-0.5,nx+0.5,-0.5,ny+0.5])
-    pylab.axis("off")
+    fig.set_size_inches(6.0, 6.0)
+    fig.tight_layout()
 
-    f = pylab.gcf()
-    f.set_size_inches(6.0,6.0)
-
-    pylab.tight_layout()
-
-    pylab.savefig("rb.png")
-    pylab.savefig("rb.eps", bbox_inches="tight", pad_inches=0)
+    fig.savefig("rb.png")
+    fig.savefig("rb.pdf", bbox_inches="tight", pad_inches=0)
 
 
-if __name__== "__main__":
-    RB()
-
-
-    
-        
+if __name__ == "__main__":
+    red_black()
