b22cbf5842da7595fb55a424f367152d3af19205
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-01 17:53:46 -0400

    update script

--- binary_exoplanets/radial_velocity/do_anim.sh
+++ binary_exoplanets/radial_velocity/do_anim.sh
@@ -2,7 +2,7 @@
 
 # make the movie
 ./radial_velocity.py
-mkmovie.py -o radial_velocity radial_velocity_????.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "radial_velocity_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 radial_velocity.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp radial_velocity_0450.png radial_velocity_preview.png
