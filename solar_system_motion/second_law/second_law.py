c2a03f8eb8693a9a97277766bd5455ac4b464c32
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-01 18:39:26 -0400

    update style

--- solar_system_motion/second_law/second_law.py
+++ solar_system_motion/second_law/second_law.py
@@ -1,15 +1,14 @@
 #!/bin/env python
 
-import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 import anim_solvers.solar_system_integrator as solar_system_integrator
 
 # illustrate Kepler's second law by showing the areas corresponding to
 # equal time intervals.
 
-# M. Zingale 
+# M. Zingale
 
 # we work in units of AU, solar masses, and years
 
@@ -34,13 +33,13 @@ def doit():
 
     # compute the period of the orbit from Kepler's law and make
     # the timestep by 1/720th of a period
-    P = math.sqrt(a**3)
+    P = np.sqrt(a**3)
 
     # put 720 steps per period (the input is steps per year)
     sol = s.integrate(720/P, P)
 
-    
-    # set up some intervals (start time, end time) to shade.  
+
+    # set up some intervals (start time, end time) to shade.
     # Note, the length of the intervals should be the same
     intervals = []
     intervals.append(interval(0, P/12))
@@ -50,32 +49,27 @@ def doit():
     for i in range(len(intervals)):
 
         if intervals[i].start < 0 or intervals[i].end > P:
-            print "ERROR: interval {} not contained in a single orbit".format(i)
+            print(f"ERROR: interval {i} not contained in a single orbit")
 
-        print "interval {}, dt = {}".format(i, intervals[i].end - intervals[i].start)
-
+        print(f"interval {i}, dt = {intervals[i].end-intervals[i].start}")
 
     # plot the orbit
-    iframe = 0
-
     for n in range(len(sol[0].x)):
 
-        pylab.clf()
+        fig, ax = plt.subplots()
+        fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
 
-        pylab.subplots_adjust(left=0.1,right=0.9,bottom=0.1,top=0.9)
-
-        ax = pylab.gca()
         ax.set_aspect("equal", "datalim")
-        pylab.axis("off")
+        ax.axis("off")
 
         # plot the foci
-        pylab.scatter([0], [0], s=350, marker=(5,1), color="k", zorder=10)
-        pylab.scatter([0], [0], s=300, marker=(5,1), color="y", zorder=10)
+        ax.scatter([0], [0], s=350, marker=(5, 1), color="k", zorder=10)
+        ax.scatter([0], [0], s=300, marker=(5, 1), color="y", zorder=10)
 
-        # plot planet 
-        pylab.plot(sol[0].x, sol[0].y, color="r")
-        pylab.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="r", zorder=10)
-        
+        # plot planet
+        ax.plot(sol[0].x, sol[0].y, color="C0")
+        ax.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="C0", zorder=10)
+
         # shade the intervals
         for i in range(len(intervals)):
 
@@ -86,31 +80,25 @@ def doit():
             for m in range(len(sol[0].x)):
 
                 if sol[0].t[m] >= intervals[i].start and \
-                   sol[0].t[m] <= min(intervals[i].end,sol[0].t[n]):
+                   sol[0].t[m] <= min(intervals[i].end, sol[0].t[n]):
                     x.append(sol[0].x[m])
                     y.append(sol[0].y[m])
 
-                elif (sol[0].t[m] > intervals[i].end):
+                elif sol[0].t[m] > intervals[i].end:
                     break
 
+            ax.fill(x, y, alpha=0.5, facecolor="C1")
 
-            pylab.fill(x, y, alpha=0.20, facecolor="b")
+        xmin = -a * (1.0 + e)
+        xmax = a * (1.0 - e)
+        dx = xmax - xmin
+        ax.axis([xmin-0.1*dx, xmax+0.1*dx, -1.5, 1.5])
+
+        fig.set_size_inches(9.6, 7.2)
+
+        fig.savefig(f"second_law_{n:04d}.png")
+        plt.close(fig)
 
 
-        xmin = -a*(1.0+e)
-        xmax = a*(1.0-e)
-        dx = xmax-xmin
-        pylab.axis([xmin-0.1*dx, xmax+0.1*dx, -1.5, 1.5])        
-
-        f = pylab.gcf()
-        f.set_size_inches(9.6,7.2)
-
-        pylab.savefig("second_law_%04d.png" % n)
-
-    
-if __name__== "__main__":
+if __name__ == "__main__":
     doit()
-
-
-    
-        
