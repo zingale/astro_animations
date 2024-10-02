891791a8a5263d0cec91acf8884d72a93496ce5a
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-02 12:23:41 -0400

    update renderings

--- binary_exoplanets/binary_stars/do_anim.sh
+++ binary_exoplanets/binary_stars/do_anim.sh
@@ -4,7 +4,7 @@
 
 # make the movie
 ./binary_stars.py --mass1 1.0 --mass2 1.0 -e 0.0 -a 10.0
-mkmovie.py -o binary_mratio=1_e=0.0 binary_star_mratio=1.00_e=0.00_????.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=1_e=0.0.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp binary_star_mratio=1.00_e=0.00_0150.png binary_mratio=1_e=0.0_preview.png
@@ -15,7 +15,7 @@ rm -f binary_star_mratio=1.00_e=0.00_????.png
 
 # make the movie
 ./binary_stars.py --mass1 2.0 --mass2 1.0 -e 0.0 -a 10.0
-mkmovie.py -o binary_mratio=2_e=0.0 binary_star_mratio=2.00_e=0.00_????.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=2_e=0.0.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp binary_star_mratio=2.00_e=0.00_0150.png binary_mratio=2_e=0.0_preview.png
@@ -26,7 +26,7 @@ rm -f binary_star_mratio=2.00_e=0.00_????.png
 
 # make the movie
 ./binary_stars.py --mass1 1.0 --mass2 1.0 -e 0.4 -a 10.0
-mkmovie.py -o binary_mratio=1_e=0.4 binary_star_mratio=1.00_e=0.40_????.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=1_e=0.4.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp binary_star_mratio=1.00_e=0.40_0150.png binary_mratio=1_e=0.4_preview.png
@@ -37,7 +37,7 @@ rm -f binary_star_mratio=1.00_e=0.40_????.png
 
 # make the movie
 ./binary_stars.py --mass1 2.0 --mass2 1.0 -e 0.4 -a 10.0
-mkmovie.py -o binary_mratio=2_e=0.4 binary_star_mratio=2.00_e=0.40_????.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=2_e=0.4.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp binary_star_mratio=2.00_e=0.40_0150.png binary_mratio=2_e=0.4_preview.png
@@ -48,7 +48,7 @@ rm -f binary_star_mratio=2.00_e=0.40_????.png
 
 # make the movie
 ./binary_stars.py --mass1 4.0 --mass2 1.0 -e 0.4 -a 10.0 --annotate
-mkmovie.py -o binary_mratio=4_e=0.4_energy binary_star_mratio=4.00_e=0.40_????_energy.png
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=4.00_e=0.40_????_energy.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=4_e=0.4_energy.mp4
 
 # store one of the frames as the preview frame for the webpage and clean-up
 cp binary_star_mratio=4.00_e=0.40_0150_energy.png binary_mratio=4_e=0.4_energy_preview.png
