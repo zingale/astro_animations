6f9b1874c56a64c9f071b3aa37721f81a2a238d8
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-02 09:01:39 -0400

    update movie to ffmpeg / make python modern

--- radioactive_decay/radioactive_decay.py
+++ radioactive_decay/radioactive_decay.py
@@ -1,11 +1,11 @@
-import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 import random
 
-# simulate radioactive decay by populating a grid with a number of
-# markers and having each marker have a 50/50 chance of "decaying"
-# each half-life
+"""simulate radioactive decay by populating a grid with a number of
+markers and having each marker have a 50/50 chance of "decaying"
+each half-life"""
+
 
 class Marker:
 
@@ -14,25 +14,22 @@ class Marker:
         # a marker is indicated by its center (xc,yc).
         self.xc = xc
         self.yc = yc
-        
+
         # the state indicates whether it is the original object (1)
         # or decayed into its daughter product (0)
         self.state = 1
-        
 
     def decay(self):
-    
+
         if self.state == 1:
-            
+
             # random.random() returns a number in the range [0.0, 1.0)
             if random.random() >= 0.5:
                 self.state = 0
 
 
 def num_decayed(markers):
-
-    nd = len([q for q in markers if q.state == 0])
-    return nd
+    return len([q for q in markers if q.state == 0])
 
 
 def radioactive_decay():
@@ -42,12 +39,11 @@ def radioactive_decay():
     ny = 50
 
     # estimate the number of half-lifes needed to decay all markers
-    nest = int(math.log(nx*ny)/math.log(2))
+    nest = int(np.log(nx*ny)/np.log(2))
 
     # allocate storage for the number of markers that are decayed at
     # each half-life.  For safety, we allocate space for 2x our estimate
-    hist_decay = numpy.zeros(2*nest)
-    
+    hist_decay = np.zeros(2*nest)
 
     # define the length of a marker side
     L = 0.8
@@ -61,13 +57,13 @@ def radioactive_decay():
     # loop over half-lives, and re-evaluate the marker state
     for t in range(2*nest):
 
-        pylab.clf()
+        fig, ax = plt.subplots()
 
         # the margins are funny -- we pick them to ensure that the
         # plot size is an integer multiple of the number of markers in
         # each dimension
-        pylab.subplots_adjust(left=0.0493333,  right=0.9506666,
-                              bottom=0.0493333,top=0.9506666)
+        fig.subplots_adjust(left=0.0493333, right=0.9506666,
+                            bottom=0.0493333, top=0.9506666)
 
         # draw the current state
         for m in markers:
@@ -77,28 +73,28 @@ def radioactive_decay():
             else:
                 c = "w"
 
-            pylab.fill([m.xc-L/2, m.xc-L/2,
-                        m.xc+L/2, m.xc+L/2,
-                        m.xc-L/2],
-                       [m.yc-L/2, m.yc+L/2,
-                        m.yc+L/2, m.yc-L/2,
-                        m.yc-L/2], 
-                       c)
+            ax.fill([m.xc-L/2, m.xc-L/2,
+                     m.xc+L/2, m.xc+L/2,
+                     m.xc-L/2],
+                    [m.yc-L/2, m.yc+L/2,
+                     m.yc+L/2, m.yc-L/2,
+                     m.yc-L/2],
+                    color=c, edgecolor="k")
 
-            
         nd = num_decayed(markers)
         hist_decay[t] = nd
 
-        ax = pylab.axis([-1,nx+1,-1,ny+1])
-        pylab.axis("off")
+        ax.axis([-1, nx+1, -1, ny+1])
+        ax.axis("off")
 
-        pylab.text(-1,-1, "number decayed = %d" % (nd),horizontalalignment="left", verticalalignment="top")
+        ax.text(-1, -1, f"number decayed = {nd}",
+                horizontalalignment="left", verticalalignment="top")
 
-        f = pylab.gcf()
-        f.set_size_inches(7.5,7.5)
+        fig.set_size_inches(7.5, 7.5)
 
-        outfile = "radioactive_decay_%04d.png" % t
-        pylab.savefig(outfile)
+        outfile = f"radioactive_decay_{t:04d}.png"
+        fig.savefig(outfile)
+        plt.close(fig)
 
         print(t, nd)
 
@@ -110,40 +106,29 @@ def radioactive_decay():
         for m in markers:
             m.decay()
 
-
     # now make a plot of the number that decayed as a function of half-life
-    pylab.clf()
-    pylab.subplots_adjust(left=0.1,   right=0.9,
-                          bottom=0.1, top=0.9)
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(left=0.1, right=0.9,
+                        bottom=0.1, top=0.9)
 
-
-    tsmooth = numpy.arange(1000)*t/1000.
+    tsmooth = np.arange(1000) * t / 1000.0
     pred = nx*ny*(1.0/2.0)**tsmooth
 
-    pylab.plot(tsmooth, pred, color="b")
+    ax.plot(tsmooth, pred, color="b")
 
-    time = numpy.arange(t-1)
-    pylab.scatter(time, nx*ny-hist_decay[0:t-1], color="b", label="parent")
-    pylab.scatter(time, hist_decay[0:t-1], color="r", label="daughter")
+    time = np.arange(t-1)
+    ax.scatter(time, nx*ny-hist_decay[0:t-1], color="b", label="parent")
+    ax.scatter(time, hist_decay[0:t-1], color="r", label="daughter")
 
-    pylab.axis([0,numpy.max(time)+1,0,1.1*nx*ny])
-    pylab.xlabel("half life")
+    ax.axis([0, np.max(time)+1, 0, 1.1*nx*ny])
+    ax.set_xlabel("half life")
 
-    leg = pylab.legend(loc=2)
-    ltext = leg.get_texts()
-    pylab.setp(ltext, fontsize='small')
-    leg.draw_frame(0)
+    leg = ax.legend(loc=2, fontsize="small", frameon=False)
 
-
-
-    outfile = "radioactive_decay_%04d.png" % (t+1)
-    pylab.savefig(outfile)
+    outfile = f"radioactive_decay_{t+1:04d}.png"
+    fig.set_size_inches(7.5, 7.5)
+    fig.savefig(outfile)
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     radioactive_decay()
-
-
-    
-        
