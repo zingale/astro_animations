69b0908182ffdfcac7194552e12e704b65168baa
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-02 20:59:39 -0400

    update style + ffmpeg

--- solar_system_motion/asteroid_resonance/asteroids.py
+++ solar_system_motion/asteroid_resonance/asteroids.py
@@ -1,6 +1,5 @@
 # illustrate the orbital resonance in between Juputer and the asteroid belt
 
-import math
 import random
 import matplotlib.pyplot as plt
 
@@ -12,7 +11,7 @@ def asteroids():
     ss = ssi.SolarSystem()
 
     # Jupiter
-    P_jupiter = 12 # years
+    P_jupiter = 12  # years
     e = 0.0
     ss.add_planet_by_period(P_jupiter, e, loc="perihelion")
 
@@ -29,49 +28,44 @@ def asteroids():
         a = random.uniform(a_min, a_max)
         e = random.uniform(0.0, 0.4)
         if n % 2 == 0:
-            loc="perihelion"
+            loc = "perihelion"
         else:
-            loc="aphelion"
-            
+            loc = "aphelion"
+
         ss.add_planet(a, e, loc=loc, rot="random")
 
     # integrate
     nsteps_per_year = 45.0
-    sol = ss.integrate(nsteps_per_year, 2*P_jupiter)
-    
-    
+    sol = ss.integrate(nsteps_per_year, 4*P_jupiter)
+
     # plots
     iframe = 0
-    
+
     for n in range(len(sol[0].t)):
 
-        plt.clf()
-        
+        fig, ax = plt.subplots()
+
         # plot the Sun
-        plt.scatter([0], [0], s=1600, marker=(20,1), color="k")
-        plt.scatter([0], [0], s=1500, marker=(20,1), color="#FFFF00")
+        plt.scatter([0], [0], s=1600, marker=(20, 1), color="k")
+        plt.scatter([0], [0], s=1500, marker=(20, 1), color="#FFFF00")
 
         # plot Jupiter
-        plt.plot(sol[0].x, sol[0].y, color="k")
-        plt.scatter([sol[0].x[n]], [sol[0].y[n]], s=500, color="k")
+        plt.plot(sol[0].x, sol[0].y, color="C1")
+        plt.scatter([sol[0].x[n]], [sol[0].y[n]], s=500, color="C1")
 
         # plot our asteroid
-        plt.plot(sol[1].x, sol[1].y, color="r")
-        plt.scatter([sol[1].x[n]], [sol[1].y[n]], s=50, color="r")
-
+        plt.plot(sol[1].x, sol[1].y, color="C0")
+        plt.scatter([sol[1].x[n]], [sol[1].y[n]], s=50, color="C0")
 
         # and the background asteroids
         for k in range(2, 2+n_asteroids):
             plt.plot(sol[k].x, sol[k].y, color="0.85", zorder=-100)
-            plt.scatter([sol[k].x[n]], [sol[k].y[n]], s=25, color="0.5", zorder=-100)
+            plt.scatter([sol[k].x[n]], [sol[k].y[n]], s=25,
+                        color="0.5")
 
+        fig.set_size_inches(7.2, 7.2)
 
-        f = plt.gcf()
-        f.set_size_inches(7.2, 7.2)
-
-        plt.axis("off")
-        
-        ax = plt.gca()
+        ax.axis("off")
         ax.set_aspect("equal", "datalim")
 
         # if jupiter is at perihelion, the pause and annotate
@@ -82,32 +76,31 @@ def asteroids():
             if sol[0].y[n]*sol[0].y[n+1] < 0.0 and sol[0].x[n] > 0.0:
                 perihelion = True
 
+        fig.subplots_adjust(left=0.025, right=0.975, bottom=0.025, top=0.975)
+
         if perihelion:
-            plt.text(0.5, 0.96,
-                     "Jupiter and our asteroid are at their closest point",
-                     horizontalalignment="center", transform=f.transFigure,
-                     fontsize="large")
+            ax.text(0.5, 0.97,
+                    "Jupiter and our asteroid are at their closest point",
+                    horizontalalignment="center", transform=fig.transFigure,
+                    fontsize="large", zorder=1000)
 
-            plt.text(0.5, 0.92,
-                     "The gravitational force on the asteroid is always",
-                     horizontalalignment="center", transform=f.transFigure)            
+            ax.text(0.5, 0.94,
+                    "The gravitational force on the asteroid is strongest",
+                    horizontalalignment="center",
+                    transform=fig.transFigure, zorder=1000)
 
-            plt.text(0.5, 0.89,
-                     "strongest and in the same direction here",            
-                     horizontalalignment="center", transform=f.transFigure)            
-            
-
-            plt.arrow(sol[1].x[n], sol[1].y[n], 1.0, 0.0, color="r",
-                      length_includes_head=True,
-                      head_width = 0.2, width=0.05, overhang=-0.1)
+            ax.arrow(sol[1].x[n], sol[1].y[n], 1.0, 0.0, color="C0",
+                     length_includes_head=True,
+                     head_width=0.2, width=0.05, overhang=-0.1)
 
             for k in range(150):
-                plt.savefig(f"asteroids_{iframe:04d}.png")
+                fig.savefig(f"asteroids_{iframe:04d}.png")
                 iframe += 1
-                 
-        plt.savefig(f"asteroids_{iframe:04d}.png")
+
+        fig.savefig(f"asteroids_{iframe:04d}.png")
+        plt.close(fig)
         iframe += 1
+
 
 if __name__ == "__main__":
     asteroids()
-    
