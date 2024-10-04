8f08c2db46fc34f2258392ba7c003c97b60a1f50
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 17:31:57 -0400

    test different encoding

--- ellipses/eccentricity/do_anim.sh
+++ ellipses/eccentricity/do_anim.sh
@@ -2,7 +2,7 @@
 
 # make the movie
 ./eccentricity.py
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "ellipse_*.png" -vcodec mpeg4 -c:v libx264 -crf 20 eccentricity.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "ellipse_*.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart eccentricity.mp4
 
 # clean-up
 rm -f ellipse_???.png
