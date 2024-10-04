3e7ff4e1d5fe0c949d1657e287ad0b56829d579f
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 08:00:06 -0400

    update style

--- solar_system_motion/orbitalenergy/orbitalenergy.py
+++ solar_system_motion/orbitalenergy/orbitalenergy.py
@@ -1,17 +1,15 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 import anim_solvers.solar_system_integrator as ss
 
-# Integrate the orbit of a planet around the Sun, and display the
-# kinetic and potential energy
-#
-# In this version, we just consider orbits with the horizontal 
-# velocity <= the circular velocity.
+"""
+Integrate the orbit of a planet around the Sun, and display the
+kinetic and potential energy
 
-
-# M. Zingale (2008-09-14)
+In this version, we just consider orbits with the horizontal 
+velocity <= the circular velocity.
+"""
 
 # we work in MKS units
 G = 6.67428e-11      # m^3 kg^{-1} s^{-2}
@@ -33,65 +31,54 @@ def orbitalenergy():
     # the timestep by 1/720th of a period
     P = orbit.period(0)
 
-    print "period = ", P/year
+    print("period = ", P/year)
 
-    sol = orbit.integrate(360, P/year)
+    sol = orbit.integrate(360, 2*P/year)
 
-
-    # ================================================================
     # plotting
-    # ================================================================
 
-    # plot the orbit
     iframe = 0
 
-    # v1
     for n in range(len(sol[0].t)):
 
-        plt.clf()
+        fig, ax = plt.subplots()
 
-        # plot the foci
-        plt.scatter([0], [0], s=250, marker=(5,1), color="k")
-        plt.scatter([0], [0], s=200, marker=(5,1), color="y")
+        # plot the Sun
+        plt.scatter([0], [0], s=1600, marker=(20, 1), color="k")
+        plt.scatter([0], [0], s=1500, marker=(20, 1), color="#FFFF00")
 
-        # plot planet 
-        plt.plot(sol[0].x, sol[0].y, color="r")
-        plt.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="r")
-
+        # plot planet
+        ax.plot(sol[0].x, sol[0].y, color="C0")
+        ax.scatter([sol[0].x[n]], [sol[0].y[n]], s=100, color="C0")
 
         # compute the kinetic energy / kg
         KE = 0.5*(sol[0].vx[n]**2 + sol[0].vy[n]**2)
 
         # compute the potential energy / kg
-        r = math.sqrt(sol[0].x[n]**2 + sol[0].y[n]**2)
+        r = np.sqrt(sol[0].x[n]**2 + sol[0].y[n]**2)
         PE = - G*M_sun/r
-        
-        plt.axis([-4*AU,2*AU,-4*AU,2*AU])
-        
-        f = plt.gcf()
-        f.set_size_inches(7.2,7.2)
 
-        ax = plt.gca()
+        ax.axis([-3.5*AU, 2*AU, -3.5*AU, 2*AU])
         ax.set_aspect("equal", "datalim")
 
-        plt.title("Orbital Energy")
+        fig.set_size_inches(7.2, 7.2)
 
-        plt.text(-3.5*AU,-3*AU,  "KE / unit mass (J/kg): %10.5e" % (KE))
-        plt.text(-3.5*AU,-3.3*AU,"PE / unit mass (J/kg): %10.5e" % (PE))
-        plt.text(-3.5*AU,-3.6*AU,"total energy / unit mass (J/kg): %10.5e" % (PE + KE))
+        ax.set_title("Orbital Energy")
 
-        plt.xlabel("x [m]")
-        plt.ylabel("y [m]")
+        ax.text(-2.5*AU, -2.2*AU,  f"KE / unit mass (J/kg): {KE:10.5e}", fontsize="13")
+        ax.text(-2.5*AU, -2.5*AU, f"PE / unit mass (J/kg): {PE:10.5e}", fontsize="13")
+        ax.text(-2.5*AU, -2.8*AU, f"total energy / unit mass (J/kg): {PE + KE:10.5e}", fontsize="13")
+
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("y [m]")
 
         ax.xaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
         ax.yaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
 
-        plt.savefig("orbitalenergy_%04d.png" % n)
+        fig.tight_layout()
+        fig.savefig(f"orbitalenergy_{n:04d}.png")
+        plt.close(fig)
 
-    
-if __name__== "__main__":
+
+if __name__ == "__main__":
     orbitalenergy()
-
-
-    
-        
