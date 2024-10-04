803bf0b3c46aeb558de4183e1d61e708c88c8496
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 19:11:18 -0400

    update rendering

--- binary_exoplanets/binary_reference/do_anim.sh
+++ binary_exoplanets/binary_reference/do_anim.sh
@@ -2,7 +2,7 @@
 
 # make the movie
 ./binary_reference.py
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "*.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_reference_mratio=4_e=0.4.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "*.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart  binary_reference_mratio=4_e=0.4.mp4
 
 rm -f binary_star_????.png
 
--- binary_exoplanets/planetary_orbit/do_anim.sh
+++ binary_exoplanets/planetary_orbit/do_anim.sh
@@ -2,7 +2,7 @@
 
 # make the movie
 ./planetary_orbits.py
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "*.png" -vcodec mpeg4 -c:v libx264 -crf 20 planetary_orbits.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "*.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart planetary_orbits.mp4
 
 rm -f planetary_orbit_????.png
 
