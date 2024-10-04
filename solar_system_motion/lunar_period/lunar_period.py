0d129b41ccb1ea520e9490811055535be7b4550d
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 08:10:02 -0400

    update animation

--- solar_system_motion/lunar_period/lunar_period.py
+++ solar_system_motion/lunar_period/lunar_period.py
@@ -1,6 +1,5 @@
 #!/bin/env python
 
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,8 +15,8 @@ P_M_sid = 27.32*s_per_day    # sidereal period of Moon
 P_M_syn = 29.53*s_per_day    # synodic period of Moon
 
 # compute angular velocities
-omega_E = 2.0*math.pi/P_E
-omega_M = 2.0*math.pi/P_M_sid
+omega_E = 2.0*np.pi/P_E
+omega_M = 2.0*np.pi/P_M_sid
 
 # orbital radii (dimensionless and scale exaggerated)
 r_E = 1.0
@@ -28,8 +27,8 @@ x_E_c = 0.0
 y_E_c = 0.0
 
 # starting phase -- for full Moon.  Sun is at the origin
-phi_E = 3.0*math.pi/2.0
-phi_M = 3.0*math.pi/2.0
+phi_E = 3.0*np.pi/2.0
+phi_M = 3.0*np.pi/2.0
 
 # number of frames
 nframes = 500
@@ -41,133 +40,112 @@ eps = 1.e-8
 
 
 def lunar_period():
+    """the main driver"""
 
     # time and timestep
     t = 0
     dt = P_M_syn / (nframes - 1)
     dt = dt*(1 - eps)    # reduce the timestep slightly to make sure
-                         # we end at the desired time
+    # we end at the desired time
 
     iframe = 0
 
     # part 0: show the initial configuration
     for n in range(nfreeze):
-
-        draw_earth_moon(t, iframe, connect_EM = 1, label = 1,
-                        annotation = "full Moon")
+        draw_earth_moon(t, iframe, connect_EM=1, label=1,
+                        annotation="full Moon")
         iframe += 1
-
 
     # part I: integrate up to sidereal period
-    while (t < P_M_sid):
-
+    while t < P_M_sid:
         draw_earth_moon(t, iframe, connect_EM = 0)
-
         t += dt
         iframe += 1
-
 
     # part II: freeze after 1 lunar sidereal period
     for n in range(nfreeze):
-
-        draw_earth_moon(t, iframe, connect_EM = 0,
-                        annotation = "one sidereal lunar orbital period later")
-
+        draw_earth_moon(t, iframe, connect_EM=0,
+                        annotation="one sidereal lunar orbital period later")
         iframe += 1
 
-
     # part III: integrate up to the synodic period -- next full moon
-    while (t <= P_M_syn):
-
-        draw_earth_moon(t, iframe, connect_EM = 0)
-
+    while t <= P_M_syn:
+        draw_earth_moon(t, iframe, connect_EM=0)
         t += dt
         iframe += 1
 
-
     # part IV: freeze after 1 lunar synodic period
     for n in range(nfreeze):
-
-        draw_earth_moon(t, iframe, connect_EM = 1,
-                        annotation = "one synodic period later, full Moon again")
-
+        draw_earth_moon(t, iframe, connect_EM=1,
+                        annotation="one synodic period later, full Moon again")
         iframe += 1
 
 
 def circle(xc, yc, r):
-    theta = np.arange(361)*2.0*math.pi/360
+    theta = np.radians(np.arange(361))
     return r*np.cos(theta) + xc, r*np.sin(theta) + yc
 
 
-def draw_earth_moon(t, iframe, connect_EM = 0, label = 0,
-                    annotation = ""):
+def draw_earth_moon(t, iframe, connect_EM=0, label=0,
+                    annotation=""):
 
-    plt.clf()
+    fig, ax = plt.subplots()
 
     # compute positions of the Earth
-    x_E = x_E_c + r_E*math.cos(omega_E*t + phi_E)
-    y_E = y_E_c + r_E*math.sin(omega_E*t + phi_E)
-
+    x_E = x_E_c + r_E*np.cos(omega_E*t + phi_E)
+    y_E = y_E_c + r_E*np.sin(omega_E*t + phi_E)
 
     # compute positions of the Moon
-    x_M = x_E + r_M*math.cos(omega_M*t + phi_M)
-    y_M = y_E + r_M*math.sin(omega_M*t + phi_M)
-
+    x_M = x_E + r_M*np.cos(omega_M*t + phi_M)
+    y_M = y_E + r_M*np.sin(omega_M*t + phi_M)
 
     # plot the Sun
-    plt.scatter([0], [0], s=1600, marker=(20,1), color="k")
-    plt.scatter([0], [0], s=1500, marker=(20,1), color="#FFFF00")
+    ax.scatter([0], [0], s=1600, marker=(20, 1), color="k")
+    ax.scatter([0], [0], s=1500, marker=(20, 1), color="#FFFF00")
 
     # plot the Earth
-    plt.scatter([x_E], [y_E], s=150, color="b")
+    ax.scatter([x_E], [y_E], s=150, color="C0")
 
     if label:
-        plt.text(1.05*x_E + 0.05, 1.05*y_E,
-                   "Earth", color="b", fontsize=12)
-
+        ax.text(1.05*x_E + 0.05, 1.05*y_E,
+                "Earth", color="C0", fontsize=12)
 
     # plot the Moon
-    plt.scatter([x_M], [y_M], s=50, color="0.5")
+    ax.scatter([x_M], [y_M], s=50, color="0.5")
 
     if label:
-        plt.text(1.05*x_M + 0.05, 1.05*y_M,
-                   "Moon", color="0.5", fontsize=12)
-
+        ax.text(1.05*x_M + 0.05, 1.05*y_M,
+                "Moon", color="0.5", fontsize=12)
 
     # plot the orbit of Earth
     xo_E, yo_E = circle(0.0, 0.0, r_E)
-    plt.plot(xo_E, yo_E, color="b", linestyle=":", lw=2)
-
+    ax.plot(xo_E, yo_E, color="C0", linestyle=":", lw=2)
 
     # plot the orbit of the Moon
     xo_M, yo_M = circle(x_E, y_E, r_M)
-    plt.plot(xo_M, yo_M, color="0.5", linestyle=":", lw=2)
-
+    ax.plot(xo_M, yo_M, color="0.5", linestyle=":", lw=2)
 
     if connect_EM:
         # draw a line connecting the Sun-Earth-Moon to show we are
         # at full Moon again
-        plt.plot([0, x_M], [0, y_M], color="0.5", linestyle=":", lw=2)
-
+        ax.plot([0, x_M], [0, y_M], color="0.5", linestyle=":", lw=2)
 
     if not annotation == "":
-        plt.text(-r_M, 1.5*r_M, annotation, fontsize=12)
+        ax.text(-r_M, 1.5*r_M, annotation, fontsize=14)
 
-    plt.axis([-(2*r_M), 1.05*(r_E+r_M),
-                -1.05*(r_E+r_M),(2*r_M)])
+    ax.axis([-(2*r_M), 1.05*(r_E+r_M),
+             -1.05*(r_E+r_M), (2*r_M)])
 
-    plt.axis("off")
-
-    ax = plt.gca()
+    ax.axis("off")
     ax.set_aspect("equal", "datalim")
 
-    plt.subplots_adjust(left=0.05,right=0.98,bottom=0.05,top=0.98)
+    fig.subplots_adjust(left=0.05, right=0.98, bottom=0.05, top=0.98)
 
-    f = plt.gcf()
-    f.set_size_inches(7.2,7.2)
+    fig.set_size_inches(7.2, 7.2)
 
-    plt.savefig("lunar_period_%04d.png" % iframe)
+    fig.savefig(f"lunar_period_{iframe:04d}.png")
+    plt.close(fig)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     lunar_period()
