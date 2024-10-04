eb9d547aa632465552510f9bc19de2b664dac89f
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 18:55:02 -0400

    fix rendering

--- binary_exoplanets/binary_stars/do_anim.sh
+++ binary_exoplanets/binary_stars/do_anim.sh
@@ -4,10 +4,8 @@
 
 # make the movie
 ./binary_stars.py --mass1 1.0 --mass2 1.0 -e 0.0 -a 10.0
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=1_e=0.0.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart binary_mratio=1_e=0.0.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp binary_star_mratio=1.00_e=0.00_0150.png binary_mratio=1_e=0.0_preview.png
 rm -f binary_star_mratio=1.00_e=0.00_????.png
 
 
@@ -15,10 +13,8 @@ rm -f binary_star_mratio=1.00_e=0.00_????.png
 
 # make the movie
 ./binary_stars.py --mass1 2.0 --mass2 1.0 -e 0.0 -a 10.0
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=2_e=0.0.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.00_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart binary_mratio=2_e=0.0.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp binary_star_mratio=2.00_e=0.00_0150.png binary_mratio=2_e=0.0_preview.png
 rm -f binary_star_mratio=2.00_e=0.00_????.png
 
 
@@ -26,10 +22,8 @@ rm -f binary_star_mratio=2.00_e=0.00_????.png
 
 # make the movie
 ./binary_stars.py --mass1 1.0 --mass2 1.0 -e 0.4 -a 10.0
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=1_e=0.4.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=1.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart binary_mratio=1_e=0.4.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp binary_star_mratio=1.00_e=0.40_0150.png binary_mratio=1_e=0.4_preview.png
 rm -f binary_star_mratio=1.00_e=0.40_????.png
 
 
@@ -37,10 +31,8 @@ rm -f binary_star_mratio=1.00_e=0.40_????.png
 
 # make the movie
 ./binary_stars.py --mass1 2.0 --mass2 1.0 -e 0.4 -a 10.0
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=2_e=0.4.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=2.00_e=0.40_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart binary_mratio=2_e=0.4.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp binary_star_mratio=2.00_e=0.40_0150.png binary_mratio=2_e=0.4_preview.png
 rm -f binary_star_mratio=2.00_e=0.40_????.png
 
 
@@ -48,9 +40,7 @@ rm -f binary_star_mratio=2.00_e=0.40_????.png
 
 # make the movie
 ./binary_stars.py --mass1 4.0 --mass2 1.0 -e 0.4 -a 10.0 --annotate
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=4.00_e=0.40_????_energy.png" -vcodec mpeg4 -c:v libx264 -crf 20 binary_mratio=4_e=0.4_energy.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "binary_star_mratio=4.00_e=0.40_????_energy.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart binary_mratio=4_e=0.4_energy.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp binary_star_mratio=4.00_e=0.40_0150_energy.png binary_mratio=4_e=0.4_energy_preview.png
 rm -f binary_star_mratio=4.00_e=0.40_????_energy.png
 
