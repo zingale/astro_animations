cf930d39d86c79185443862b3d2e22b8136cdd98
Michael Zingale <michael.zingale@stonybrook.edu>
2024-10-04 19:23:38 -0400

    update rendering

--- binary_exoplanets/radial_velocity/do_anim.sh
+++ binary_exoplanets/radial_velocity/do_anim.sh
@@ -2,9 +2,7 @@
 
 # make the movie
 ./radial_velocity.py
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "radial_velocity_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 radial_velocity.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "radial_velocity_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart radial_velocity.mp4
 
-# store one of the frames as the preview frame for the webpage and clean-up
-cp radial_velocity_0450.png radial_velocity_preview.png
 rm -f radial_velocity_????.png
 
--- binary_exoplanets/radial_velocity_ell/do_anim.sh
+++ binary_exoplanets/radial_velocity_ell/do_anim.sh
@@ -2,10 +2,7 @@
 
 # make the movie
 ./radial_velocity_ell.py
-ffmpeg -framerate 15 -f image2 -pattern_type glob -i "radial_velocity_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 radial_velocity_ell.mp4
+ffmpeg -framerate 15 -f image2 -pattern_type glob -i "radial_velocity_????.png" -vcodec mpeg4 -c:v libx264 -crf 20 -pix_fmt yuv420p -movflags +faststart radial_velocity_ell.mp4
 
-
-# store one of the frames as the preview frame for the webpage and clean-up
-cp radial_velocity_0450.png radial_velocity_ell_preview.png
 rm -f radial_velocity_????.png
 
